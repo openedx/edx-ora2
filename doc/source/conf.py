49049b90296399507428bcee657ce596e3ecf9c8
Mark Hoeber <hoeber@edx.org>
2014-02-03 10:27:24 -0500

    Added requirements file for napoleon

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -32,7 +32,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.base")
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.ifconfig', 'sphinx.ext.todo']
+extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.napoleon', 'sphinx.ext.coverage', 'sphinx.ext.ifconfig', 'sphinx.ext.todo']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
