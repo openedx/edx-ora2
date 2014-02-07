3aacdf994938e1829e11d5fc30c30642184f8c9a
David Ormsbee <dave@edx.org>
2014-02-07 13:50:13 -0500

    Make Submission Admin page more useful by inlining student item and scores.

--- apps/submissions/admin.py
+++ apps/submissions/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 
 from submissions.models import Score, StudentItem, Submission
 
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = (
+        'student_item', 'attempt_number', 'submitted_at', 'created_at', 'answer',
+        'scores'
+    )
+
+    def scores(self, obj):
+        return ", ".join(
+            "{}/{}".format(score.points_earned, score.points_possible)
+            for score in Score.objects.filter(submission=obj.id)
+        )
+
 admin.site.register(Score)
 admin.site.register(StudentItem)
-admin.site.register(Submission)
+admin.site.register(Submission, SubmissionAdmin)
