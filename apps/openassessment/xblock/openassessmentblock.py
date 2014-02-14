13b9eaae967b1d008166d4e0ae075c27a4b86714
Stephen Sanchez <steve@edx.org>
2014-02-14 10:48:34 -0500

    Removing html tags from prompt

--- apps/openassessment/xblock/openassessmentblock.py
+++ apps/openassessment/xblock/openassessmentblock.py
@@ -19,22 +19,19 @@ mako_default_filters = ['unicode', 'h', 'trim']
 
 
 DEFAULT_PROMPT = """
-    <h3>Censorship in the Libraries</h3>
+    Censorship in the Libraries
 
-    <p>'All of us can think of a book that we hope none of our children or any
+    'All of us can think of a book that we hope none of our children or any
     other children have taken off the shelf. But if I have the right to remove
     that book from the shelf -- that work I abhor -- then you also have exactly
     the same right and so does everyone else. And then we have no books left on
     the shelf for any of us.' --Katherine Paterson, Author
-    </p>
 
-    <p>
     Write a persuasive essay to a newspaper reflecting your views on censorship
     in libraries. Do you believe that certain materials, such as books, music,
     movies, magazines, etc., should be removed from the shelves if they are
     found offensive? Support your position with convincing arguments from your
     own experience, observations, and/or reading.
-    </p>
 """
 
 DEFAULT_RUBRIC_INSTRUCTIONS = "Read for conciseness, clarity of thought, and form."
