df4529120cb05ff1b35b4b30d5c5645a5784c9bb
Brian Talbot <btalbot@edx.org>
2014-03-02 17:04:13 -0500

    Peer Assessment Template/Styling Work: * adding basic styling for rubric-based UI * applying rubric-based UI styling to peer assessment * adding oa-specific Sass extend partial * basic template indentation cleanup * adding initial JS for all collapsed/expanded UI to use * refactoring small Sass bits
    TIM-129

--- apps/openassessment/xblock/static/js/src/design.js
+++ apps/openassessment/xblock/static/js/src/design.js
@@ -5,16 +5,21 @@
 // * this is merely for UI behavior and any work here should be folded into production-level JavaScript files and methods.
 
 function $linkNewWindow(e) {
-  window.open($(e.target).attr('href'));
   e.preventDefault();
+  window.open($(e.target).attr('href'));
+}
+
+function $toggleExpansion(e) {
+    e.preventDefault();
+    $(e.target).closest('.ui-toggle-visibility').toggleClass('is--collapsed');
 }
 
 // --------------------
 
 jQuery(document).ready(function($) {
 
-  // removing no-js, accessibility/modernizr marker
-  $('html').removeClass('no-js');
+  // collapse/expand UI
+  $('.ui-toggle-visibility ui-toggle-visibility__control').bind('click', toggleExpansion);
 
   // general link management - new window/tab
   $('a[data-rel="external"]').bind('click', $linkNewWindow);
