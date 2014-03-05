7e40ea0e687979744cc640d1efc200a31d25c4cb
David Ormsbee <dave@edx.org>
2014-03-05 17:53:25 -0500

    Add Django Debug Toolbar to default dev config.

--- settings/dev.py
+++ settings/dev.py
@@ -5,10 +5,14 @@ Dev-specific Django settings.
 # Inherit from base settings
 from .base import *
 
-MIDDLEWARE_CLASSES += (
-    'django_pdb.middleware.PdbMiddleware',  # Needed to enable shell-on-crash behavior
-)
-
 INSTALLED_APPS += (
     'django_pdb',            # Allows post-mortem debugging on exceptions
+    'debug_toolbar',
 )
+
+MIDDLEWARE_CLASSES += (
+    'django_pdb.middleware.PdbMiddleware',  # Needed to enable shell-on-crash behavior
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INTERNAL_IPS = ('127.0.0.1',)
