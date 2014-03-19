70bfb6fdf84bc77fae3c3f3ec2726754f966a8c5
Will Daly <will@edx.org>
2014-03-19 16:21:14 -0400

    Update fixtures for new submission field format; add test for fixture import

--- settings/test.py
+++ settings/test.py
@@ -21,6 +21,19 @@ NOSE_ARGS = [
     '--cover-erase',
     ]
 
+# This will still use an in-memory database for the unit tests,
+# but will create an on-disk database for testing migrations/fixture installation.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'testdb',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 # Configure lettuce
