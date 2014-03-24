ef62ec649119d3814dbf2c459992df711d2e6d01
Stephen Sanchez <steve@edx.org>
2014-03-24 13:10:53 -0400

    Wiring the appropriate templates into the peer section UI

--- apps/openassessment/xblock/peer_assessment_mixin.py
+++ apps/openassessment/xblock/peer_assessment_mixin.py
@@ -165,11 +165,9 @@ class PeerAssessmentMixin(object):
                 path = 'openassessmentblock/peer/oa_peer_turbo_mode.html'
                 context_dict["peer_submission"] = peer_sub
             else:
-                path = 'openassessmentblock/peer/oa_peer_complete.html'
-        elif workflow.get("status") == "done":
+                path = 'openassessmentblock/peer/oa_peer_waiting.html'
+        elif workflow.get("status") == "done" or finished:
             path = "openassessmentblock/peer/oa_peer_complete.html"
-        elif finished:
-            path = 'openassessmentblock/peer/oa_peer_waiting.html'
 
         return self.render_assessment(path, context_dict)
 
