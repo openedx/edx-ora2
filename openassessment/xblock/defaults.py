f5a43b979a3e6cf2dd25bc11d4e0775f9ef25540
gradyward <gward@edx.or>
2014-07-21 11:55:26 -0400

    Sets Default problem to include Labels for Criteria and Options

--- openassessment/xblock/defaults.py
+++ openassessment/xblock/defaults.py
@@ -15,43 +15,45 @@ DEFAULT_PROMPT = """
 DEFAULT_RUBRIC_CRITERIA = [
     {
         'name': "Ideas",
+        'label': "Ideas",
         'prompt': "Determine if there is a unifying theme or main idea.",
         'order_num': 0,
         'feedback': 'optional',
         'options': [
             {
-                'order_num': 0, 'points': 0, 'name': 'Poor',
+                'order_num': 0, 'points': 0, 'name': 'Poor', 'label': 'Poor',
                 'explanation': """Difficult for the reader to discern the main idea.  Too brief or too repetitive to establish or maintain a focus."""
             },
             {
-                'order_num': 1, 'points': 3, 'name': 'Fair',
+                'order_num': 1, 'points': 3, 'name': 'Fair', 'label': 'Fair',
                 'explanation': """Presents a unifying theme or main idea, but may include minor tangents.  Stays somewhat focused on topic and task."""
             },
             {
-                'order_num': 2, 'points': 5, 'name': 'Good',
+                'order_num': 2, 'points': 5, 'name': 'Good', 'label': 'Good',
                 'explanation': """Presents a unifying theme or main idea without going off on tangents.  Stays completely focused on topic and task."""
             },
         ],
     },
     {
         'name': "Content",
+        'label': "Content",
         'prompt': "Assess the content of the submission",
         'order_num': 1,
         'options': [
             {
-                'order_num': 0, 'points': 0, 'name': 'Poor',
+                'order_num': 0, 'points': 0, 'name': 'Poor', 'label': 'Poor',
                 'explanation': """Includes little information with few or no details or unrelated details.  Unsuccessful in attempts to explore any facets of the topic."""
             },
             {
-                'order_num': 1, 'points': 1, 'name': 'Fair',
+                'order_num': 1, 'points': 1, 'name': 'Fair', 'label': 'Fair',
                 'explanation': """Includes little information and few or no details.  Explores only one or two facets of the topic."""
             },
             {
-                'order_num': 2, 'points': 3, 'name': 'Good',
+                'order_num': 2, 'points': 3, 'name': 'Good', 'label': 'Good',
                 'explanation': """Includes sufficient information and supporting details. (Details may not be fully developed; ideas may be listed.)  Explores some facets of the topic."""
             },
             {
-                'order_num': 3, 'points': 3, 'name': 'Excellent',
+                'order_num': 3, 'points': 3, 'name': 'Excellent', 'label': 'Excellent',
                 'explanation': """Includes in-depth information and exceptional supporting details that are fully developed.  Explores all facets of the topic."""
             },
         ],
