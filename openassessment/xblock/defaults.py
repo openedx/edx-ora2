6f3bb7086148c10c272b4ec5a7e2d9e588717f10
gradyward <gward@edx.or>
2014-07-22 10:23:47 -0400

    Allowed narrow window editing, and ensured solid transitions

--- openassessment/xblock/defaults.py
+++ openassessment/xblock/defaults.py
@@ -63,7 +63,7 @@ DEFAULT_RUBRIC_CRITERIA = [
 # The rubric's feedback prompt is a set of instructions letting the student
 # know they can provide additional free form feedback in their assessment.
 DEFAULT_RUBRIC_FEEDBACK_PROMPT = """
-    (Optional) What aspects of this response stood out to you? What did it do well? How could it improve?
+(Optional) What aspects of this response stood out to you? What did it do well? How could it improve?
 """
 
 DEFAULT_EXAMPLE_ANSWER = """
