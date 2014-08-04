01a326dd0c6b10d91a17430efcfd99fca528ccdd
Will Daly <will@edx.org>
2014-08-04 10:29:38 -0400

    Remove toggle visibility

--- openassessment/xblock/static/js/src/oa_leaderboard.js
+++ openassessment/xblock/static/js/src/oa_leaderboard.js
@@ -27,47 +27,9 @@ OpenAssessment.LeaderboardView.prototype = {
             function(html) {
                 // Load the HTML and install event handlers
                 $('#openassessment__leaderboard', view.element).replaceWith(html);
-                view.installHandlers();
             }
         ).fail(function(errMsg) {
             baseView.showLoadError('leaderboard', errMsg);
         });
     },
-
-    /**
-    Install event handlers for the view.
-    **/
-    installHandlers: function() {
-        // Install a click handler for collapse/expand
-        var sel = $('#openassessment__leaderboard', this.element);
-        this.baseView.setUpCollapseExpand(sel);
-    },
-
-    /**
-    Hide elements, including setting the aria-hidden attribute for screen readers.
-
-    Args:
-        sel (JQuery selector): The selector matching elements to hide.
-        hidden (boolean): Whether to hide or show the elements.
-
-    Returns:
-        undefined
-    **/
-    setHidden: function(sel, hidden) {
-        sel.toggleClass('is--hidden', hidden);
-        sel.attr('aria-hidden', hidden ? 'true' : 'false');
-    },
-
-    /**
-    Check whether elements are hidden.
-
-    Args:
-        sel (JQuery selector): The selector matching elements to hide.
-
-    Returns:
-        boolean
-    **/
-    isHidden: function(sel) {
-        return sel.hasClass('is--hidden') && sel.attr('aria-hidden') == 'true';
-    }
 };
