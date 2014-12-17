6aeaeded79699c63d76bddd92dd68b1ed04afd33
muzaffaryousaf <muzaffar.yousaf@arbisoft.com>
2014-12-17 17:03:16 +0500

    Fixing the failing test.
    TNL-900

--- openassessment/xblock/test/test_staff_info.py
+++ openassessment/xblock/test/test_staff_info.py
@@ -523,8 +523,6 @@ class TestCourseStaff(XBlockHandlerTestCase):
         xblock.xmodule_runtime = self._create_mock_runtime(
             xblock.scope_ids.usage_id, False, False, "Bob"
         )
-        xblock.xmodule_runtime.user_is_staff = False
-
         resp = self.request(xblock, 'cancel_submission', json.dumps({}))
         self.assertIn("you do not have permission", resp.decode('utf-8').lower())
 
@@ -539,8 +537,15 @@ class TestCourseStaff(XBlockHandlerTestCase):
     def test_cancel_submission_full_flow(self, xblock):
         # Simulate that we are course staff
         xblock.xmodule_runtime = self._create_mock_runtime(
-            xblock.scope_ids.usage_id, True, False, "Bob"
+            xblock.scope_ids.usage_id, False, False, "Bob"
         )
+
+        resp = self.request(xblock, 'cancel_submission', json.dumps({}))
+        self.assertIn("you do not have permission", resp.decode('utf-8').lower())
+
+        # If we ARE course staff, then we should see the cancel submission option
+        # with valid error message.
+        xblock.xmodule_runtime.user_is_staff = True
 
         bob_item = STUDENT_ITEM.copy()
         bob_item["item_id"] = xblock.scope_ids.usage_id
