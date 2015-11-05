f1e79b27fca6a250e880d956eb37d80c1f46fb9c
Andy Armstrong <andya@edx.org>
2015-11-05 16:28:17 -0500

    First phase of staff re-grading UI

--- openassessment/xblock/static/js/src/lms/oa_staff_area.js
+++ openassessment/xblock/static/js/src/lms/oa_staff_area.js
@@ -1,5 +1,6 @@
 (function(OpenAssessment) {
     'use strict';
+
     /**
      * Interface for staff area view.
      *
@@ -20,24 +21,22 @@
     OpenAssessment.StaffAreaView.prototype = {
 
         /**
-         Load the staff area.
-         **/
+         * Load the staff area.
+         */
         load: function() {
             var view = this;
 
             // If we're course staff, the base template should contain a section
             // for us to render the staff area into.  If that doesn't exist,
             // then we're not staff, so we don't need to send the AJAX request.
-            if ($('#openassessment__staff-area', view.element).length > 0) {
-                this.server.render('staff_area').done(
-                    function(html) {
+            if ($('.openassessment__staff-area', view.element).length > 0) {
+                this.server.render('staff_area')
+                    .done(function(html) {
                         // Load the HTML and install event handlers
-                        $('#openassessment__staff-area', view.element).replaceWith(html);
-                        view.server.renderLatex($('#openassessment__staff-area', view.element));
+                        $('.openassessment__staff-area', view.element).replaceWith(html);
+                        view.server.renderLatex($('.openassessment__staff-area', view.element));
                         view.installHandlers();
-                    }
-                ).fail(
-                    function() {
+                    }).fail(function() {
                         view.baseView.showLoadError('staff_area');
                     }
                 );
@@ -45,62 +44,104 @@
         },
 
         /**
-         Upon request, loads the student info section of the staff info view. This
-         allows viewing all the submissions and assessments associated to the given
-         student's current workflow.
-         **/
-        loadStudentInfo: function() {
+         * Upon request, loads the student info section of the staff area.
+         * This allows viewing all the submissions and assessments associated
+         * to the given student's current workflow.
+         *
+         * @param options An optional set of options to render the section.
+         * @returns {promise} A promise representing the successful oading
+         * of the student info section.
+         */
+        loadStudentInfo: function(options) {
             var view = this;
-            var sel = $('#openassessment__staff-tools', this.element);
-            var student_username = sel.find('#openassessment__student_username').val();
-            this.server.studentInfo(student_username).done(
-                function(html) {
-                    // Load the HTML and install event handlers
-                    $('#openassessment__student-info', view.element).replaceWith(html);
+            var $staffTools = $('.openassessment__staff-tools', this.element);
+            var $form = $staffTools.find('.openassessment_student_info_form');
+            var student_username = $staffTools.find('.openassessment__student_username').val();
+            var showFormError = function(errorMessage) {
+                $form.find('.form--error').text(errorMessage);
+            };
+            var deferred = $.Deferred();
 
-                    // Install key handler for new staff grade Save button.
-                    var selCancelSub = $('#openassessment__staff-info__cancel__submission', view.element);
-                    selCancelSub.on('click', '#submit_cancel_submission', function (eventObject) {
+            // Clear any previous student information
+            $('.openassessment__student-info', view.element).text('');
+
+            if (student_username.trim()) {
+                this.server.studentInfo(student_username, options)
+                    .done(function(html) {
+                        // Clear any error message
+                        showFormError('');
+
+                        // Load the HTML and install event handlers
+                        $('.openassessment__student-info', view.element).replaceWith(html);
+
+                        // Install key handler for cancel submission button.
+                        $staffTools.on('click', '.action--submit-cancel-submission', function (eventObject) {
                             eventObject.preventDefault();
                             view.cancelSubmission($(this).data('submission-uuid'));
+                        });
+
+                        // Install change handler for textarea (to enable cancel submission button)
+                        var handleChange = function(eventData) { view.handleCommentChanged(eventData); };
+                        $staffTools.find('.cancel_submission_comments')
+                            .on('change keyup drop paste', handleChange);
+
+
+                        // Initialize the rubric
+                        var $rubric = $('.staff-assessment__assessment', view.element);
+                        if ($rubric.size() > 0) {
+                            var rubricElement = $rubric.get(0);
+                            var rubric = new OpenAssessment.Rubric(rubricElement);
+
+                            // Install a change handler for rubric options to enable/disable the submit button
+                            rubric.canSubmitCallback($.proxy(view.staffSubmitEnabled, view));
+
+                            // Install a click handler for the submit button
+                            $('.wrapper--staff-assessment .action--submit', view.element).click(
+                                function(eventObject) {
+                                    var target = $(eventObject.currentTarget),
+                                        rootElement = target.closest('.openassessment__student-info'),
+                                        submissionID = rootElement.data('submission-uuid');
+
+                                    eventObject.preventDefault();
+                                    view.submitStaffAssessment(submissionID, rubric);
+                                }
+                            );
                         }
-                    );
-
-                    // Install change handler for textarea (to enable cancel submission button)
-                    var handleChange = function(eventData) { view.handleCommentChanged(eventData); };
-                    selCancelSub.find('#staff-info__cancel-submission__comments')
-                        .on('change keyup drop paste', handleChange);
-
-                }
-            ).fail(
-                function() {
-                    view.showLoadError('student_info');
-                }
-            );
+                        deferred.resolve();
+                    })
+                    .fail(function() {
+                        showFormError(gettext('Unexpected server error.'));
+                        deferred.reject();
+                    });
+            } else {
+                showFormError(gettext('A learner name must be provided.'));
+                deferred.reject();
+            }
+            return deferred.promise();
         },
 
         /**
-         Install event handlers for the view.
-         **/
+         * Install event handlers for the view.
+         */
         installHandlers: function() {
-            var $staffArea = $('#openassessment__staff-area', this.element);
-            var toolsElement = $('#openassessment__staff-tools', $staffArea);
-            var infoElement = $('#openassessment__student-info', $staffArea);
             var view = this;
+            var $staffArea = $('.openassessment__staff-area', this.element);
+            var $staffTools = $('.openassessment__staff-tools', $staffArea);
+            var $staffInfo =  $('.openassessment__student-info', $staffArea);
 
-            if (toolsElement.length <= 0) {
+            if ($staffArea.length <= 0) {
                 return;
             }
 
-            this.baseView.setUpCollapseExpand(toolsElement, function() {});
-            this.baseView.setUpCollapseExpand(infoElement, function() {});
+            this.baseView.setUpCollapseExpand($staffTools, function() {});
+            this.baseView.setUpCollapseExpand($staffInfo, function() {});
 
             // Install a click handler for the staff button panel
             $staffArea.find('.ui-staff__button').click(
                 function(eventObject) {
                     var $button = $(eventObject.currentTarget),
-                        panelID = $button.data('panel'),
-                        $panel = $staffArea.find('#' + panelID).first();
+                        panelClass = $button.data('panel'),
+                        $panel = $staffArea.find('.' + panelClass).first();
                     if ($button.hasClass('is--active')) {
                         $button.removeClass('is--active');
                         $panel.addClass('is--hidden');
@@ -124,7 +165,7 @@
             );
 
             // Install key handler for student id field
-            toolsElement.find('#openassessment_student_info_form').submit(
+            $staffTools.find('.openassessment_student_info_form').submit(
                 function(eventObject) {
                     eventObject.preventDefault();
                     view.loadStudentInfo();
@@ -132,7 +173,7 @@
             );
 
             // Install a click handler for requesting student info
-            toolsElement.find('#submit_student_username').click(
+            $staffTools.find('.action--submit-username').click(
                 function(eventObject) {
                     eventObject.preventDefault();
                     view.loadStudentInfo();
@@ -140,7 +181,7 @@
             );
 
             // Install a click handler for scheduling AI classifier training
-            toolsElement.find('#schedule_training').click(
+            $staffTools.find('.action--submit-training').click(
                 function(eventObject) {
                     eventObject.preventDefault();
                     view.scheduleTraining();
@@ -148,7 +189,7 @@
             );
 
             // Install a click handler for rescheduling unfinished AI tasks for this problem
-            toolsElement.find('#reschedule_unfinished_tasks').click(
+            $staffTools.find('.action--submit-unfinished-tasks').click(
                 function(eventObject) {
                     eventObject.preventDefault();
                     view.rescheduleUnfinishedTasks();
@@ -157,108 +198,148 @@
         },
 
         /**
-         Sends a request to the server to schedule the training of classifiers for
-         this problem's Example Based Assessments.
-
-         **/
+         * Sends a request to the server to schedule the training
+         * of classifiers for this problem's Example Based Assessments.
+         */
         scheduleTraining: function() {
             var view = this;
-            this.server.scheduleTraining().done(
-                function(msg) {
-                    $('#schedule_training_message', view.element).text(msg);
-                }
-            ).fail(function(errMsg) {
-                    $('#schedule_training_message', view.element).text(errMsg);
-                });
+            this.server.scheduleTraining()
+                .done(function(msg) {
+                    $('.schedule_training_message', view.element).text(msg);
+                }).fail(function(errMsg) {
+                $('.schedule_training_message', view.element).text(errMsg);
+            });
         },
 
         /**
-         Begins the process of rescheduling all unfinished grading tasks. This incdludes
-         checking if the classifiers have been created, and grading any unfinished
-         student submissions.
-
-         **/
+         * Begins the process of rescheduling all unfinished grading tasks.
+         * This includes checking if the classifiers have been created,
+         * and grading any unfinished student submissions.
+         */
         rescheduleUnfinishedTasks: function() {
             var view = this;
-            this.server.rescheduleUnfinishedTasks().done(
-                function(msg) {
-                    $('#reschedule_unfinished_tasks_message', view.element).text(msg);
-                }
-            ).fail(function(errMsg) {
-                    $('#reschedule_unfinished_tasks_message', view.element).text(errMsg);
-                });
+            this.server.rescheduleUnfinishedTasks()
+                .done(function(msg) {
+                    $('.reschedule_unfinished_tasks_message', view.element).text(msg);
+                }).fail(function(errMsg) {
+                $('.reschedule_unfinished_tasks_message', view.element).text(errMsg);
+            });
         },
 
         /**
-         Upon request, cancel the submission from grading pool.
-         **/
+         * Upon request, cancel the submission from grading pool.
+         */
         cancelSubmission: function(submissionUUID) {
             // Immediately disable the button to prevent multiple requests.
             this.cancelSubmissionEnabled(false);
             var view = this;
-            var sel = $('#openassessment__student-info', this.element);
-            var comments = sel.find('#staff-info__cancel-submission__comments').val();
-            this.server.cancelSubmission(submissionUUID, comments).done(
-                function(msg) {
+            var comments = $('.cancel_submission_comments', this.element).val();
+            this.server.cancelSubmission(submissionUUID, comments)
+                .done(function(msg) {
                     $('.cancel-submission-error').html('');
-                    $('#openassessment__staff-info__cancel__submission', view.element).html(msg);
-                }
-            ).fail(function(errMsg) {
+                    view.loadStudentInfo({expanded_view: 'final-grade'})
+                        .done(function() {
+                            $('.openassessment__staff-info__cancel__submission', view.element).html(msg);
+                        });
+                })
+                .fail(function(errMsg) {
                     $('.cancel-submission-error').html(errMsg);
                 });
         },
 
         /**
-         Enable/disable the cancel submission button.
-         Check whether the cancel submission button is enabled.
-
-         Args:
-         enabled (bool): If specified, set the state of the button.
-
-         Returns:
-         bool: Whether the button is enabled.
-
-         Examples:
-         >> view.submitEnabled(true);  // enable the button
-         >> view.submitEnabled();  // check whether the button is enabled
-         >> true
-         **/
+         * Enable/disable the cancel submission button.
+         *
+         * Check whether the cancel submission button is enabled.
+         *
+         * Args:
+         *   enabled (bool): If specified, set the state of the button.
+         *
+         * Returns:
+         *   bool: Whether the button is enabled.
+         *
+         * Examples:
+         * >> view.submitEnabled(true);  // enable the button
+         * >> view.submitEnabled();  // check whether the button is enabled
+         * >> true
+         */
         cancelSubmissionEnabled: function(enabled) {
-            var sel = $('#submit_cancel_submission', this.element);
+            var $cancelButton = $('.action--submit-cancel-submission', this.element);
             if (typeof enabled === 'undefined') {
-                return !sel.hasClass('is--disabled');
+                return !$cancelButton.hasClass('is--disabled');
             } else {
-                sel.toggleClass('is--disabled', !enabled);
+                $cancelButton.toggleClass('is--disabled', !enabled);
             }
         },
 
         /**
-         Set the comment text.
-         Retrieve the comment text.
-
-         Args:
-         text (string): reason to .
-
-         Returns:
-         string: The current comment text.
-         **/
+         * Set the comment text.
+         *
+         * Retrieve the comment text.
+         *
+         * Args:
+         *   text (string): reason to .
+         *
+         * Returns:
+         *   string: The current comment text.
+         */
         comment: function(text) {
-            var sel = $('#staff-info__cancel-submission__comments', this.element);
+            var $submissionComments = $('.cancel_submission_comments', this.element);
             if (typeof text === 'undefined') {
-                return sel.val();
+                return $submissionComments.val();
             } else {
-                sel.val(text);
+                $submissionComments.val(text);
             }
         },
 
         /**
-         Enable/disable the cancel submission based on whether
-         the user has entered a comment.
-         **/
+         * Enable/disable the cancel submission based on whether
+         * the user has entered a comment.
+         */
         handleCommentChanged: function() {
             // Enable the cancel submission button only for non-blank comments
-            var isBlank = ($.trim(this.comment()) !== '');
+            var isBlank = $.trim(this.comment()) !== '';
             this.cancelSubmissionEnabled(isBlank);
+        },
+
+
+        /**
+         * Enable/disable the staff assessment submit button.
+         *
+         * @param enabled If specified, sets the state of the button.
+         * @returns {boolean} Whether the button is enabled
+         */
+        staffSubmitEnabled: function(enabled) {
+            var button = $('.wrapper--staff-assessment .action--submit', this.element);
+            if (typeof enabled === 'undefined') {
+                return !button.hasClass('is--disabled');
+            } else {
+                button.toggleClass('is--disabled', !enabled);
+            }
+        },
+
+        /**
+         * Submit the staff assessment.
+         *
+         * @param rubric The rubric being assessed.
+         */
+        submitStaffAssessment: function(submissionID, rubric) {
+            // Send the assessment to the server
+            var view = this;
+            var baseView = this.baseView;
+            baseView.toggleActionError('staff', null);
+            view.staffSubmitEnabled(false);
+
+            this.server.staffAssess(
+                rubric.optionsSelected(), rubric.criterionFeedback(), rubric.overallFeedback(), submissionID
+                )
+                .done(function() {
+                    view.loadStudentInfo({expanded_view:  'final-grade'});
+                })
+                .fail(function(errorMessage) {
+                    baseView.toggleActionError('staff', errorMessage);
+                    view.staffSubmitEnabled(true);
+                });
         }
     };
 })(OpenAssessment);
