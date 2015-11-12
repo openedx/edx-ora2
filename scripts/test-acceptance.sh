c69c00af717789d78d5fed49b0c4a39617a8f04a
Christine Lytwynec <clytwynec@edx.org>
2015-11-12 10:49:04 -0500

    add custom a11y rules to bok choy tests

--- scripts/test-acceptance.sh
+++ scripts/test-acceptance.sh
@@ -33,5 +33,24 @@ cd test/logs
 
 test_name="${1:-acceptance}"
 
+
+# The machines that these tests run on in jenkins have an old
+# version of npm which doesn't seem to work with the
+# requirements in this repo. There is a devops ticket for
+# this, but it may be better to wait until some of the other
+# work they have going on now is done before attempting to
+# install the custom rules via npm. This bit of code is a
+# work around for that. It will work both in jenkins and for
+# local runs of the tests.
+if [[ "${test_name}" = "accessibility" ]]; then
+    export BOKCHOY_A11Y_CUSTOM_RULES_FILE=../../test/custom_a11y_rules.js
+
+    if [[ ! -f $BOKCHOY_A11Y_CUSTOM_RULES_FILE ]]; then
+        echo "Custom a11y rules file not found. Fetching it..."
+        custom_a11y_version=8633ea2fd04d84d69c4610bbfbf38db32ad005a9
+        curl https://raw.githubusercontent.com/edx/edx-custom-a11y-rules/${custom_a11y_version}/lib/custom_a11y_rules.js > ${BOKCHOY_A11Y_CUSTOM_RULES_FILE}
+    fi
+fi
+
 echo "Running acceptance tests from ${test_name}.py against the sandbox..."
 python ../acceptance/${test_name}.py
