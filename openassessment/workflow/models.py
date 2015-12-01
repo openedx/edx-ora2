17caa6b14826c5578877f6b167242fd637d7769b
Eric Fischer <efischer@edx.org>
2015-12-01 14:30:12 -0500

    Change exception type
    I misunderstood what was happening in my cursory investigation earlier -
    all that needs to change here is the type of exception being caught.

--- openassessment/workflow/models.py
+++ openassessment/workflow/models.py
@@ -382,7 +382,7 @@ class AssessmentWorkflow(TimeStampedModel, StatusModel):
         # A staff step must always be available, to allow for staff overrides
         try:
             self.steps.get(name=self.STATUS.staff)
-        except AttributeError:
+        except AssessmentWorkflowStep.DoesNotExist:
             for step in list(self.steps.all()):
                 step.order_num += 1
             self.steps.add(
