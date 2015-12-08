13aed6b34a91024532221ea71bbd0d408bd28cae
cahrens <christina@edx.org>
2015-12-08 10:30:10 -0500

    Accessibility and acceptance bok choy tests.

--- test/acceptance/accessibility.py
+++ test/acceptance/accessibility.py
@@ -3,7 +3,7 @@ UI-level acceptance tests for OpenAssessment accessibility.
 """
 import os
 import unittest
-from tests import OpenAssessmentTest, StaffAreaPage
+from tests import OpenAssessmentTest, StaffAreaPage, FullWorkflowMixin
 
 
 class OpenAssessmentA11yTest(OpenAssessmentTest):
@@ -47,6 +47,12 @@ class SelfAssessmentA11yTest(OpenAssessmentA11yTest):
         self.submission_page.visit()
         self._check_a11y(self.submission_page)
 
+        # TODO: There is an accessibility error on the self-review form due to TNL-3882.
+        # Commenting out test code so the check doesn't have to be disabled for the whole page.
+        # self.submission_page.visit().submit_response(self.SUBMISSION)
+        # self.self_asmnt_page.wait_for_page().wait_for_response()
+        # self._check_a11y(self.self_asmnt_page)
+
 
 class PeerAssessmentA11yTest(OpenAssessmentA11yTest):
     """
@@ -57,8 +63,18 @@ class PeerAssessmentA11yTest(OpenAssessmentA11yTest):
         super(PeerAssessmentA11yTest, self).setUp('peer_only')
 
     def test_peer_assessment_a11y(self):
+        # Create a submission for one learner.
         self.submission_page.visit()
         self._check_a11y(self.submission_page)
+        self.submission_page.visit().submit_response(self.SUBMISSION)
+
+        # Create a submission for a second learner.
+        self.auto_auth_page.visit()
+        self.submission_page.visit().submit_response(self.SUBMISSION)
+
+        # Check accessibility on the peer assessment page (there should be at least one available).
+        self.peer_asmnt_page.visit()
+        self._check_a11y(self.peer_asmnt_page)
 
 
 class StudentTrainingA11yTest(OpenAssessmentA11yTest):
@@ -71,6 +87,11 @@ class StudentTrainingA11yTest(OpenAssessmentA11yTest):
     def test_student_training_a11y(self):
         self.submission_page.visit()
         self._check_a11y(self.submission_page)
+
+        # Check accessibility on the training page.
+        self.submission_page.visit().submit_response(self.SUBMISSION)
+        self.student_training_page.wait_for_page().wait_for_response()
+        self._check_a11y(self.student_training_page)
 
 
 class StaffAreaA11yTest(OpenAssessmentA11yTest):
@@ -135,9 +156,59 @@ class StaffAreaA11yTest(OpenAssessmentA11yTest):
         self.self_asmnt_page.wait_for_page().wait_for_response()
         self.self_asmnt_page.assess("self", self.OPTIONS_SELECTED).wait_for_complete()
         self.assertTrue(self.self_asmnt_page.is_complete)
-        self._verify_staff_grade_section("COMPLETE", None)
+        self._verify_staff_grade_section(self.STAFF_OVERRIDE_EXISTS, None)
 
         self._check_a11y(self.staff_asmnt_page)
+
+
+class FullWorkflowA11yTest(OpenAssessmentA11yTest, FullWorkflowMixin):
+    """
+    Test accessibility at the end of a "full workflow" problem. In particular,
+    this verifies the accessibility of the "Your Grade" section and the related
+    sections in staff tools when all assessment steps have been completed.
+    """
+
+    def setUp(self):
+        super(FullWorkflowA11yTest, self).setUp('full_workflow', staff=True)
+        self.staff_area_page = StaffAreaPage(self.browser, self.problem_loc)
+
+    def test_training_peer_self_staff_override(self):
+        """
+        """
+        # Create a learner with submission, training, and self assessment completed.
+        learner = self.do_submission_training_self_assessment(self.LEARNER_EMAIL, self.LEARNER_PASSWORD)
+
+        # Now create a second learner so that learner 1 has someone to assess.
+        # The second learner does all the steps as well (submission, training, self assessment, peer assessment).
+        self.do_submission_training_self_assessment("learner2@foo.com", None)
+        self.do_peer_assessment(options=self.PEER_ASSESSMENT)
+
+        # Go back to the first learner to complete her workflow.
+        self.login_user(learner)
+
+        # Learner 1 does peer assessment of learner 2 to complete workflow.
+        self.do_peer_assessment(options=self.SUBMITTED_ASSESSMENT)
+
+        # Continue grading by other students if necessary to ensure learner has a peer grade.
+        self.verify_submission_has_peer_grade(learner)
+
+        # At this point, the learner sees the peer assessment score (0). Verify the accessibility
+        # of the "your grade" section.
+        self.assertEqual(self.PEER_ASSESSMENT_SCORE, self.grade_page.wait_for_page().score)
+        self._check_a11y(self.grade_page)
+
+        # Now do a staff override, changing the score (to 1).
+        self.do_staff_override(learner)
+
+        # Refresh and check the accessibility of "your grade" section again.
+        self.browser.refresh()
+        self.assertEqual(self.STAFF_OVERRIDE_SCORE, self.grade_page.wait_for_page().score)
+        self._check_a11y(self.grade_page)
+
+        # Also verify the accessibility of the complete staff area information.
+        self.staff_area_page.visit().show_learner(learner)
+        self.staff_area_page.expand_learner_report_sections()
+        self._check_a11y(self.staff_area_page)
 
 
 if __name__ == "__main__":
