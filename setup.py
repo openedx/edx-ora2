521a1997bf63afb54542b36d4c605596ae786495
Eric Fischer <efischer@edx.org>
2017-02-24 14:04:31 -0500

    version bump

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='ora2',
-    version='1.2.1',
+    version='1.2.2',
     author='edX',
     url='http://github.com/edx/edx-ora2',
     description='edx-ora2',
