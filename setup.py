945450a9b1c32615d9d5212e70eccd3a9655f260
Eric Fischer <efischer@edx.org>
2017-07-20 15:51:20 -0400

    1.4.7, patch release for edx-submissions

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='ora2',
-    version='1.4.6',
+    version='1.4.7',
     author='edX',
     url='http://github.com/edx/edx-ora2',
     description='edx-ora2',
