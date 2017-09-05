8f679064ebbb0db3d7f271e58a8d6d65a8ff3989
Eric Fischer <efischer@edx.org>
2017-09-05 14:06:11 -0400

    Version 2.1.0

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='ora2',
-    version='2.0.6',
+    version='2.1.0',
     author='edX',
     url='http://github.com/edx/edx-ora2',
     description='edx-ora2',
