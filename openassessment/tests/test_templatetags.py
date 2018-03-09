bca89f005a4762cc24108fed40a91ee18b8d5e7d
Hasnain <hasnain.naveed123@gmail.com>
2018-03-09 19:48:33 +0500

    ....

--- openassessment/tests/test_templatetags.py
+++ openassessment/tests/test_templatetags.py
@@ -9,24 +9,22 @@ class OAExtrasTests(unittest.TestCase):
 
     @ddt.data(
         ("", ""),
-        ('https://dummy-url.com', 'href="https://dummy-url.com"'),
-        ('http://dummy-url.com', 'href="http://dummy-url.com"'),
-        ('http://dummy-url.org', 'href="http://dummy-url.org"'),
-        ('http://dummy-url.ag', 'href="http://dummy-url.ag"'),
-        ('dummy-url.com', 'href="http://dummy-url.com"')
+        ('Check this https://dummy-url.com for details', 'https://dummy-url.com'),
+    #     ('http://dummy-url.com', 'href="http://dummy-url.com"'),
+    #     ('http://dummy-url.org', 'href="http://dummy-url.org"'),
+    #     ('http://dummy-url.ag', 'href="http://dummy-url.ag"'),
+    #     ('dummy-url.com', 'href="http://dummy-url.com"')
     )
     @ddt.unpack
-    def test_link_and_linebreak(self, text, expected_text):
+    def test_link_and_linebreak(self, text, link_text):
         template = Template(
             "{% load oa_extras %}"
             "{{ text|link_and_linebreak }}"
         )
         rendered_template = template.render(Context({'text': text}))
-        self.assertIn(expected_text, rendered_template)
+        self.assertIn(link_text, rendered_template)
         if text:
-            self.assertTrue(bool(
-                re.search(
-                    "<a.*target.*href.*</a>",
-                    rendered_template
-                )
-            ))
+            self.assertRegexpMatches(
+                rendered_template,
+                r'<a.*target.*>{link_text}</a>'.format(link_text=link_text),
+            )
