30c16aea85d6970dac3fe7c254cf18f5ca149b9f
Diego <rojastorrado@gmail.com>
2018-03-15 11:30:59 -0500

    Marking and extracting strings with utget_lazy

--- openassessment/xblock/data_conversion.py
+++ openassessment/xblock/data_conversion.py
@@ -4,6 +4,17 @@ Data Conversion utility methods for handling ORA2 XBlock data transformations an
 """
 import json
 
+from django.utils.functional import Promise
+from django.utils.encoding import force_text
+from django.core.serializers.json import DjangoJSONEncoder
+
+
+class LazyEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Promise):
+            return force_text(obj)
+        return super(LazyEncoder, self).default(obj)
+
 
 def convert_training_examples_list_to_dict(examples_list):
     """
@@ -119,9 +130,13 @@ def create_prompts_list(prompt_or_serialized_prompts):
     if prompt_or_serialized_prompts is None:
         prompt_or_serialized_prompts = ''
 
-    try:
-        prompts = json.loads(prompt_or_serialized_prompts)
-    except ValueError:
+    if type(prompt_or_serialized_prompts) is dict:
+        # Lazy translation objects cannot be parsed to json normally
+        # https://docs.djangoproject.com/en/1.8/topics/serialization/#serialization-formats-json
+        # we need to pass it with a custom encoder.
+        decode_prompts = json.dumps(prompt_or_serialized_prompts, cls=LazyEncoder)
+        prompts = json.loads(decode_prompts)
+    else:
         prompts = [
             {
                 'description': prompt_or_serialized_prompts,
--- openassessment/xblock/defaults.py
+++ openassessment/xblock/defaults.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=line-too-long
 
-DEFAULT_PROMPT = """
+from django.utils.translation import ugettext_lazy
+
+
+DEFAULT_PROMPT = ugettext_lazy("""
     Censorship in the Libraries
 
     'All of us can think of a book that we hope none of our children or any other children have taken off the shelf. But if I have the right to remove that book from the shelf -- that work I abhor -- then you also have exactly the same right and so does everyone else. And then we have no books left on the shelf for any of us.' --Katherine Paterson, Author
@@ -10,51 +13,51 @@ DEFAULT_PROMPT = """
     Write a persuasive essay to a newspaper reflecting your views on censorship in libraries. Do you believe that certain materials, such as books, music, movies, magazines, etc., should be removed from the shelves if they are found offensive? Support your position with convincing arguments from your own experience, observations, and/or reading.
 
     Read for conciseness, clarity of thought, and form.
-"""
+  """)
 
 DEFAULT_RUBRIC_CRITERIA = [
     {
-        'name': "Ideas",
-        'label': "Ideas",
-        'prompt': "Determine if there is a unifying theme or main idea.",
+        'name': ugettext_lazy("Ideas"),
+        'label': ugettext_lazy("Ideas"),
+        'prompt': ugettext_lazy("Determine if there is a unifying theme or main idea."),
         'order_num': 0,
-        'feedback': 'optional',
+        'feedback': ugettext_lazy('optional'),
         'options': [
             {
-                'order_num': 0, 'points': 0, 'name': 'Poor', 'label': 'Poor',
-                'explanation': """Difficult for the reader to discern the main idea.  Too brief or too repetitive to establish or maintain a focus."""
+                'order_num': 0, 'points': 0, 'name': ugettext_lazy('Poor'), 'label': ugettext_lazy('Poor'),
+                'explanation': ugettext_lazy("""Difficult for the reader to discern the main idea.  Too brief or too repetitive to establish or maintain a focus.""")
             },
             {
-                'order_num': 1, 'points': 3, 'name': 'Fair', 'label': 'Fair',
-                'explanation': """Presents a unifying theme or main idea, but may include minor tangents.  Stays somewhat focused on topic and task."""
+                'order_num': 1, 'points': 3, 'name': ugettext_lazy('Fair'), 'label': ugettext_lazy('Fair'),
+                'explanation': ugettext_lazy("""Presents a unifying theme or main idea, but may include minor tangents.  Stays somewhat focused on topic and task.""")
             },
             {
-                'order_num': 2, 'points': 5, 'name': 'Good', 'label': 'Good',
-                'explanation': """Presents a unifying theme or main idea without going off on tangents.  Stays completely focused on topic and task."""
+                'order_num': 2, 'points': 5, 'name': ugettext_lazy('Good'), 'label': ugettext_lazy('Good'),
+                'explanation': ugettext_lazy("""Presents a unifying theme or main idea without going off on tangents.  Stays completely focused on topic and task.""")
             },
         ],
     },
     {
-        'name': "Content",
-        'label': "Content",
-        'prompt': "Assess the content of the submission",
+        'name': ugettext_lazy("Content"),
+        'label': ugettext_lazy("Content"),
+        'prompt': ugettext_lazy("Assess the content of the submission"),
         'order_num': 1,
         'options': [
             {
-                'order_num': 0, 'points': 0, 'name': 'Poor', 'label': 'Poor',
-                'explanation': """Includes little information with few or no details or unrelated details.  Unsuccessful in attempts to explore any facets of the topic."""
+                'order_num': 0, 'points': 0, 'name': ugettext_lazy('Poor'), 'label': ugettext_lazy('Poor'),
+                'explanation': ugettext_lazy("""Includes little information with few or no details or unrelated details.  Unsuccessful in attempts to explore any facets of the topic.""")
             },
             {
-                'order_num': 1, 'points': 1, 'name': 'Fair', 'label': 'Fair',
-                'explanation': """Includes little information and few or no details.  Explores only one or two facets of the topic."""
+                'order_num': 1, 'points': 1, 'name': ugettext_lazy('Fair'), 'label': ugettext_lazy('Fair'),
+                'explanation': ugettext_lazy("""Includes little information and few or no details.  Explores only one or two facets of the topic.""")
             },
             {
-                'order_num': 2, 'points': 3, 'name': 'Good', 'label': 'Good',
-                'explanation': """Includes sufficient information and supporting details. (Details may not be fully developed; ideas may be listed.)  Explores some facets of the topic."""
+                'order_num': 2, 'points': 3, 'name': ugettext_lazy('Good'), 'label': ugettext_lazy('Good'),
+                'explanation': ugettext_lazy("""Includes sufficient information and supporting details. (Details may not be fully developed; ideas may be listed.)  Explores some facets of the topic.""")
             },
             {
-                'order_num': 3, 'points': 3, 'name': 'Excellent', 'label': 'Excellent',
-                'explanation': """Includes in-depth information and exceptional supporting details that are fully developed.  Explores all facets of the topic."""
+                'order_num': 3, 'points': 3, 'name': ugettext_lazy('Excellent'), 'label': ugettext_lazy('Excellent'),
+                'explanation': ugettext_lazy("""Includes in-depth information and exceptional supporting details that are fully developed.  Explores all facets of the topic.""")
             },
         ],
     },
@@ -62,30 +65,31 @@ DEFAULT_RUBRIC_CRITERIA = [
 
 # The rubric's feedback prompt is a set of instructions letting the student
 # know they can provide additional free form feedback in their assessment.
-DEFAULT_RUBRIC_FEEDBACK_PROMPT = """
+DEFAULT_RUBRIC_FEEDBACK_PROMPT = ugettext_lazy("""
 (Optional) What aspects of this response stood out to you? What did it do well? How could it be improved?
-"""
+""")
+
 
 # The rubric's feedback text is the default text displayed and used as
 # the student's response to the feedback prompt
-DEFAULT_RUBRIC_FEEDBACK_TEXT = """
+DEFAULT_RUBRIC_FEEDBACK_TEXT = ugettext_lazy("""
 I think that this response...
-"""
+""")
 
 DEFAULT_EXAMPLE_ANSWER = (
-    "Replace this text with your own sample response for this assignment. "
+    ugettext_lazy("Replace this text with your own sample response for this assignment. "
     "Then, under Response Score to the right, select an option for each criterion. "
     "Learners practice performing peer assessments by assessing this response and comparing "
-    "the options that they select in the rubric with the options that you specified."
+    "the options that they select in the rubric with the options that you specified.")
 )
 
 DEFAULT_EXAMPLE_ANSWER_2 = (
-    "Replace this text with another sample response, "
-    "and then specify the options that you would select for this response."
+    ugettext_lazy("Replace this text with another sample response, "
+    "and then specify the options that you would select for this response.")
 )
 
 DEFAULT_STUDENT_TRAINING = {
-    "name": "student-training",
+    "name": ugettext_lazy("student-training"),
     "start": None,
     "due": None,
     "examples": [
@@ -93,12 +97,12 @@ DEFAULT_STUDENT_TRAINING = {
             "answer": DEFAULT_EXAMPLE_ANSWER,
             "options_selected": [
                 {
-                    "criterion": "Ideas",
-                    "option": "Fair"
+                    "criterion": ugettext_lazy("Ideas"),
+                    "option": ugettext_lazy("Fair")
                 },
                 {
-                    "criterion": "Content",
-                    "option": "Good"
+                    "criterion": ugettext_lazy("Content"),
+                    "option": ugettext_lazy("Good")
                 }
             ]
         },
@@ -106,12 +110,12 @@ DEFAULT_STUDENT_TRAINING = {
             "answer": DEFAULT_EXAMPLE_ANSWER_2,
             "options_selected": [
                 {
-                    "criterion": "Ideas",
-                    "option": "Poor"
+                    "criterion": ugettext_lazy("Ideas"),
+                    "option": ugettext_lazy("Poor")
                 },
                 {
-                    "criterion": "Content",
-                    "option": "Good"
+                    "criterion": ugettext_lazy("Content"),
+                    "option": ugettext_lazy("Good")
                 }
             ]
         }
@@ -125,7 +129,7 @@ DEFAULT_DUE = "2029-01-01T00:00"
 # configured. If no configuration is specified, this is the default assessment
 # module(s) associated with the XBlock.
 DEFAULT_PEER_ASSESSMENT = {
-    "name": "peer-assessment",
+    "name": ugettext_lazy("peer-assessment"),
     "start": DEFAULT_START,
     "due": DEFAULT_DUE,
     "must_grade": 5,
@@ -133,13 +137,13 @@ DEFAULT_PEER_ASSESSMENT = {
 }
 
 DEFAULT_SELF_ASSESSMENT = {
-    "name": "self-assessment",
+    "name": ugettext_lazy("self-assessment"),
     "start": DEFAULT_START,
     "due": DEFAULT_DUE,
 }
 
 DEFAULT_STAFF_ASSESSMENT = {
-    "name": "staff-assessment",
+    "name": ugettext_lazy("staff-assessment"),
     "start": DEFAULT_START,
     "due": DEFAULT_DUE,
     "required": False,
@@ -153,8 +157,8 @@ DEFAULT_ASSESSMENT_MODULES = [
 ]
 
 DEFAULT_EDITOR_ASSESSMENTS_ORDER = [
-    "student-training",
-    "peer-assessment",
-    "self-assessment",
-    "staff-assessment",
+    ugettext_lazy("student-training"),
+    ugettext_lazy("peer-assessment"),
+    ugettext_lazy("self-assessment"),
+    ugettext_lazy("staff-assessment"),
 ]
