d669aa529c84dd6c6c42dbd7b895555d5e25f15a
Ayub khan <muhammadayubkhan6@gmail.com>
2019-07-23 17:26:49 +0500

    -bump version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='ora2',
-    version='2.2.5',
+    version='2.2.6',
     author='edX',
     url='http://github.com/edx/edx-ora2',
     description='edx-ora2',
@@ -47,7 +47,10 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
     ],
     packages=find_packages(include=['openassessment*'], exclude=['*.test', '*.tests']),
     include_package_data=True,
