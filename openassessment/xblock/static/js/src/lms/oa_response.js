b5ffe5c14b9497d0eaeb8f72c9281e00a54acce3
Nathan Sprenkle <nsprenkle@edx.org>
2019-12-10 15:09:47 -0500

    Remove unused code

--- openassessment/xblock/static/js/src/lms/oa_response.js
+++ openassessment/xblock/static/js/src/lms/oa_response.js
@@ -110,11 +110,7 @@ OpenAssessment.ResponseView.prototype = {
                 var channel = pusher.subscribe("private-" + blockId);
 
                 channel.bind('client-text-edit', function(value) {
-                    // save the current position
-                    var currentCursorPosition = getCaretCharacterOffsetWithin(doc);
                     doc.value = value;
-                    // set the previous cursor position
-                    setCaretPosition(doc, currentCursorPosition);
                 });
                 channel.bind('pusher:subscription_succeeded', function() {
                     resolve(channel);
@@ -127,56 +123,6 @@ OpenAssessment.ResponseView.prototype = {
                 doc.addEventListener('input', triggerChange);
             });
         }
-
-        function getCaretCharacterOffsetWithin(element) {
-            var caretOffset = 0;
-            var doc = element.ownerDocument || element.document;
-            var win = doc.defaultView || doc.parentWindow;
-            var sel;
-            if (typeof win.getSelection != "undefined") {
-              sel = win.getSelection();
-              if (sel.rangeCount > 0) {
-                var range = win.getSelection().getRangeAt(0);
-                var preCaretRange = range.cloneRange();
-                preCaretRange.selectNodeContents(element);
-                preCaretRange.setEnd(range.endContainer, range.endOffset);
-                caretOffset = preCaretRange.toString().length;
-              }
-            } else if ( (sel = doc.selection) && sel.type != "Control") {
-              var textRange = sel.createRange();
-              var preCaretTextRange = doc.body.createTextRange();
-              preCaretTextRange.moveToElementText(element);
-              preCaretTextRange.setEndPoint("EndToEnd", textRange);
-              caretOffset = preCaretTextRange.text.length;
-            }
-            return caretOffset;
-          }
-
-          function setCaretPosition(el, pos) {
-            // Loop through all child nodes
-            for (var node of el.childNodes) {
-              if (node.nodeType == 3) { // we have a text node
-                if (node.length >= pos) {
-                    // finally add our range
-                    var range = document.createRange(),
-                        sel = window.getSelection();
-                    range.setStart(node,pos);
-                    range.collapse(true);
-                    sel.removeAllRanges();
-                    sel.addRange(range);
-                    return -1; // we are done
-                } else {
-                  pos -= node.length;
-                }
-              } else {
-                pos = setCaretPosition(node,pos);
-                if (pos == -1) {
-                    return -1; // no need to finish the for loop
-                }
-              }
-            }
-            return pos; // needed because of recursion stuff
-          }
 
         // Get CSRF token from the cookie
         var csrfToken = getCookie('csrftoken');
