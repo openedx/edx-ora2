983b7df25f2c5e44230c2064bcc1c382e5f414d6
Nathan Sprenkle <nsprenkle@edx.org>
2020-04-02 15:10:24 -0400

    Add test for get team workflow

--- openassessment/workflow/test/test_team_api.py
+++ openassessment/workflow/test/test_team_api.py
@@ -12,3 +12,20 @@ class TestTeamAssessmentWorkflowApi(CacheResetTest):
         team_workflow = team_api.create_workflow(team_submission_uuid)
 
         assert team_workflow is not None
+
+    def test_get_workflow(self):
+        # Given a workflow
+        team_submission_uuid = 'foo'
+        TeamAssessmentWorkflow.objects.create(
+            team_submission_uuid=team_submission_uuid,
+            status=AssessmentWorkflow.STATUS.waiting,
+            course_id='test course',
+            item_id='test item'
+        )
+
+        # When I get the workflow
+        team_workflow = team_api.get_workflow_for_submission(team_submission_uuid)
+
+        # It is updated and returns a correctly serialized version
+        assert team_workflow['team_submission_uuid'] == team_submission_uuid
+        assert 'staff' in team_workflow['status_details']
