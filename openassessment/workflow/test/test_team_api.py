7101bb040c474bb3fc08e202abe07bbf95145f5f
Nathan Sprenkle <nsprenkle@edx.org>
2020-04-02 18:14:01 -0400

    Clean up team workflow tests

--- openassessment/workflow/test/test_team_api.py
+++ openassessment/workflow/test/test_team_api.py
@@ -10,37 +10,23 @@ class TestTeamAssessmentWorkflowApi(CacheResetTest):
     """ Test team assessment workflow API """
 
     def test_create_workflow(self):
-        team_submission_uuid = 'foo'
-
-    def test_create_workflow(self):
         # Given a test UUID
         # When I create a workflow
-        team_workflow = team_api.create_workflow(self.team_submission_uuid)
+        team_workflow = team_api.create_workflow('foo')
 
-        # This is stubbed for now
+        # TODO - Complete in https://openedx.atlassian.net/browse/EDUCATOR-4986
         assert team_workflow is not None
 
     def test_get_workflow(self):
         # Given a workflow
-        self._create_test_workflow(self.team_submission_uuid)
+        self._create_test_workflow('foo')
 
         # When I get the workflow
-        team_workflow = team_api.get_workflow_for_submission(self.team_submission_uuid)
+        team_workflow = team_api.get_workflow_for_submission('foo')
 
         # It is updated and returns a correctly serialized version
-        assert team_workflow['team_submission_uuid'] == self.team_submission_uuid
+        assert team_workflow['team_submission_uuid'] == 'foo'
         assert 'staff' in team_workflow['status_details']
-
-    def test_cancel_workflow(self):
-        # Given a workflow
-        self._create_test_workflow(self.team_submission_uuid)
-
-        # When I cancel the workflow, for fun
-        team_api.cancel_workflow(self.team_submission_uuid, "Cancellation comment", "Cancelled by ID")
-
-        # Then workflow is cancelled...
-        # status for workflow should be cancelled...
-        # and score points_earned should be 0.
 
     def test_get_status_counts(self):
         # Initial counts should be 0
