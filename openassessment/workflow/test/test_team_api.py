898974fb676e8e114779e031345f2f34540bacb1
Nathan Sprenkle <nsprenkle@edx.org>
2020-04-02 16:58:42 -0400

    Refactor tests

--- openassessment/workflow/test/test_team_api.py
+++ openassessment/workflow/test/test_team_api.py
@@ -9,45 +9,40 @@ class TestTeamAssessmentWorkflowApi(CacheResetTest):
     def test_create_workflow(self):
         team_submission_uuid = 'foo'
 
-        team_workflow = team_api.create_workflow(team_submission_uuid)
+    def test_create_workflow(self):
+        # Given a test UUID
+        # When I create a workflow
+        team_workflow = team_api.create_workflow(self.team_submission_uuid)
 
+        # This is stubbed for now
         assert team_workflow is not None
 
     def test_get_workflow(self):
         # Given a workflow
-        team_submission_uuid = 'foo'
-        TeamAssessmentWorkflow.objects.create(
-            team_submission_uuid=team_submission_uuid,
-            status=AssessmentWorkflow.STATUS.waiting,
-            course_id='test course',
-            item_id='test item'
-        )
+        self._create_test_worfkflow(self.team_submission_uuid)
 
         # When I get the workflow
-        team_workflow = team_api.get_workflow_for_submission(team_submission_uuid)
+        team_workflow = team_api.get_workflow_for_submission(self.team_submission_uuid)
 
         # It is updated and returns a correctly serialized version
-        assert team_workflow['team_submission_uuid'] == team_submission_uuid
+        assert team_workflow['team_submission_uuid'] == self.team_submission_uuid
         assert 'staff' in team_workflow['status_details']
 
     def test_cancel_workflow(self):
         # Given a workflow
-        team_submission_uuid = 'foo'
-        TeamAssessmentWorkflow.objects.create(
-            team_submission_uuid=team_submission_uuid,
-            status=AssessmentWorkflow.STATUS.waiting,
-            course_id='test course',
-            item_id='test item'
-        )
+        self._create_test_worfkflow(self.team_submission_uuid)
 
         # When I cancel the workflow, for fun
-        team_api.cancel_workflow(
-            team_submission_uuid=team_submission_uuid,
-            comments="For fun",
-            cancelled_by_id="My ID"
-        )
+        team_api.cancel_workflow(self.team_submission_uuid, "Cancellation comment", "Cancelled by ID")
 
         # Then workflow is cancelled...
         # status for workflow should be cancelled...
         # and score points_earned should be 0.
 
+    def _create_test_worfkflow(self, team_submission_uuid):
+        return TeamAssessmentWorkflow.objects.create(
+            team_submission_uuid=team_submission_uuid,
+            status=AssessmentWorkflow.STATUS.waiting,
+            course_id='test course',
+            item_id='test item'
+        )
