7ad755165c184463d0cde7125f4638a62db93a56
atesker <andrewtr@gmail.com>
2020-04-06 17:05:41 -0400

    EDUCATOR-4980

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='ora2',
-    version='2.6.27',
+    version='2.6.28',
     author='edX',
     author_email='oscm@edx.org',
     url='http://github.com/edx/edx-ora2',
