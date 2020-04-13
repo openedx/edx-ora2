b4cbd2c70a865b3acb9f4e8b115038e38f4e2d3e
Nathan Sprenkle <nsprenkle@edx.org>
2020-04-13 17:43:32 -0400

    Add submitter is finished

--- openassessment/assessment/api/teams.py
+++ openassessment/assessment/api/teams.py
@@ -6,3 +6,19 @@ from __future__ import absolute_import
 import logging
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+def submitter_is_finished(team_submission_uuid, staff_requirements):  # pylint: disable=unused-argument
+    """
+    Determine if the submitter has finished their requirements for staff
+    assessment. Always returns True.
+
+    Args:
+        team_submission_uuid (str): Not used.
+        staff_requirements (dict): Not used.
+
+    Returns:
+        True
+
+    """
+    return True
