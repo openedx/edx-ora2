3486c3bd061777c776cdfad535e9506e7c652bed
Nathan Sprenkle <nsprenkle@edx.org>
2020-04-13 17:42:42 -0400

    Stub teams assessment API

---
+++ openassessment/assessment/api/teams.py
@@ -0,0 +1,8 @@
+"""
+Public interface for staff grading of team assignments, used by students/course staff.
+"""
+from __future__ import absolute_import
+
+import logging
+
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
