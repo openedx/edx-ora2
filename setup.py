0c60eaf61ee46cbc076495cae39776d50b3d82a0
jansenk <jkantor@edx.org>
2020-05-29 12:06:15 -0400

    versions

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='ora2',
-    version='2.7.7',
+    version='2.7.8',
     author='edX',
     author_email='oscm@edx.org',
     url='http://github.com/edx/edx-ora2',
