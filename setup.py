b455226931871545565bb4ad3ff1331910a0396f
Zulqarnain <muhammad.zulqarnain@arbisoft.com>
2020-06-16 20:14:11 +0500

    move xblock-sdk from base.in to test.in

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='ora2',
-    version='2.8.2',
+    version='2.8.3',
     author='edX',
     author_email='oscm@edx.org',
     url='http://github.com/edx/edx-ora2',
