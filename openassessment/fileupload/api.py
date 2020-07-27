e2fdf8bb762176486e30f44f7866c1522c576dbf
Nathan Sprenkle <nsprenkle@edx.org>
2020-07-27 17:45:17 -0400

    Remove old API function

--- openassessment/fileupload/api.py
+++ openassessment/fileupload/api.py
@@ -102,17 +102,6 @@ def can_delete_file(current_user_id, teams_enabled, key, team_id=None, shared_fi
     return True
 
 
-def delete_shared_files_for_user(student_id, course_id, item_id):
-    """
-    Delete shared files for a user for this block
-    """
-    uploads = SharedFileUpload.by_student_course_item(student_id, course_id, item_id)
-
-    for upload in uploads:
-        remove_file(upload.file_key)
-        upload.delete()
-
-
 def delete_shared_files_for_team(course_id, item_id, team_id):
     """
     Delete shared files for a team for this block
