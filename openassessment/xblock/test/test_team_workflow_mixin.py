3058bab52daeac2ceab541a6fb20d710170105a3
jansenk <jkantor@edx.org>
2020-08-17 16:29:54 -0400

    team workflow tests

--- openassessment/xblock/test/test_team_workflow_mixin.py
+++ openassessment/xblock/test/test_team_workflow_mixin.py
@@ -84,33 +84,19 @@ class TestTeamWorkflowMixin(TestCase):
         self.test_block.get_team_workflow_status_counts()
         api_mock.get_status_counts.assert_called()
 
-    @patch('openassessment.xblock.team_workflow_mixin.team_sub_api.get_team_submission_for_team')
-    def test_get_team_submission_uuid(self, mock_get_team_sub):
+    @patch('openassessment.xblock.team_workflow_mixin.team_sub_api.get_team_submission_for_student')
+    def test_get_team_submission_uuid(self, mock_get_team_sub_for_student):
         team_submission_uuid = 'this-is-the-uuid'
-        mock_get_team_sub.return_value = {'team_submission_uuid': team_submission_uuid}
+        mock_get_team_sub_for_student.return_value = {'team_submission_uuid': team_submission_uuid}
 
         self.assertEqual(self.test_block.get_team_submission_uuid(), team_submission_uuid)
-        mock_get_team_sub.assert_called_with(
-            STUDENT_ITEM_DICT['course_id'],
-            STUDENT_ITEM_DICT['item_id'],
-            self.test_block.team.team_id
-        )
+        mock_get_team_sub_for_student.assert_called_with(STUDENT_ITEM_DICT)
 
-    @patch('openassessment.xblock.team_workflow_mixin.team_sub_api.get_team_submission_for_team')
-    def test_get_team_submission_uuid_no_team(self, mock_get_team_sub):
-        self.test_block._has_team = False  # pylint: disable=protected-access
+    @patch('openassessment.xblock.team_workflow_mixin.team_sub_api.get_team_submission_for_student')
+    def test_get_team_submission_uuid_no_team(self, mock_get_team_sub_for_student):
+        mock_get_team_sub_for_student.side_effect = TeamSubmissionNotFoundError()
         self.assertIsNone(self.test_block.get_team_submission_uuid())
-        mock_get_team_sub.assert_not_called()
-
-    @patch('openassessment.xblock.team_workflow_mixin.team_sub_api.get_team_submission_for_team')
-    def test_get_team_submission_uuid_error(self, mock_get_team_sub):
-        mock_get_team_sub.side_effect = TeamSubmissionNotFoundError()
-        self.assertIsNone(self.test_block.get_team_submission_uuid())
-        mock_get_team_sub.assert_called_with(
-            STUDENT_ITEM_DICT['course_id'],
-            STUDENT_ITEM_DICT['item_id'],
-            self.test_block.team.team_id
-        )
+        mock_get_team_sub_for_student.assert_called_with(STUDENT_ITEM_DICT)
 
     @patch('{}.team_workflow_api.get_assessment_workflow_cancellation'.format(MODULE))
     def test_get_team_workflow_cancellation_info_no_info(self, mock_get_cancellation):
