c4705abb2acd34c41338ab02dccdfd677daacf5b
Ben Warzeski <bwarzeski@edx.org>
2020-08-31 16:17:28 -0400

    template updates for setttings

--- openassessment/xblock/static/js/src/studio/oa_edit_settings.js
+++ openassessment/xblock/static/js/src/studio/oa_edit_settings.js
@@ -59,6 +59,10 @@ OpenAssessment.EditSettingsView = function(element, assessmentViews, data) {
             if (peerAssessment.isEnabled()) {
                 self.setHidden($('#peer_assessment_schedule_editor', peerAssessment.scheduleElement), false);
             }
+
+            self.setHidden($("#openassessment_leaderboard_wrapper .disabled-label"), true);
+            self.setHidden($("#openassessment_leaderboard_wrapper .teams-warning"), true);
+            $("#openassessment_leaderboard_editor").prop("disabled", false);
         } else {
             self.setHidden(teamsetElement, false);
 
@@ -69,6 +73,9 @@ OpenAssessment.EditSettingsView = function(element, assessmentViews, data) {
             self.setHidden($('#self_assessment_schedule_editor', selfAssessment.scheduleElement), true);
             self.setHidden($('#peer_assessment_schedule_editor', peerAssessment.scheduleElement), true);
 
+            self.setHidden($("#openassessment_leaderboard_wrapper .disabled-label"), false);
+            self.setHidden($("#openassessment_leaderboard_wrapper .teams-warning"), false);
+            $("#openassessment_leaderboard_editor").prop("disabled", true)
             staffAssessment.isEnabled(true);
         }
     }
