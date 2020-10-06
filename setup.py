c0377f63cd7aea3ccd0b88aa1b084c3146f4aa4a
Nathan Sprenkle <nsprenkle@edx.org>
2020-10-06 13:25:11 -0400

    Bump ORA to 2.9.17

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='ora2',
-    version='2.9.16',
+    version='2.9.17',
     author='edX',
     author_email='oscm@edx.org',
     url='http://github.com/edx/edx-ora2',
