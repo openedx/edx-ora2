7c159a77d004400f33aa0bae5ab7357191e04326
Ben Warzeski <bwarzeski@edx.org>
2020-10-19 16:01:19 -0400

    remove debug logging

--- openassessment/xblock/static/js/src/lms_index.js
+++ openassessment/xblock/static/js/src/lms_index.js
@@ -6,7 +6,6 @@ import {
   StaffAssessmentBlock,
 } from './lms/oa_base';
 
-console.log('Webpack ORA - LMS');
 window.OpenAssessmentBlock = OpenAssessmentBlock;
 window.CourseOpenResponsesListingBlock = CourseOpenResponsesListingBlock;
 window.StaffAssessmentBlock = StaffAssessmentBlock;
