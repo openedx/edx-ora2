90f66291d050686c695f19e056ea1bee3c109d62
Luis Moreno <luis.moreno@edunext.co>
2020-11-02 11:19:06 -0400

    Do not use waffle for activation of mobile support due to it prevents us from collecting the static assets while building docker images.
    Remove waffle switch test for mobile support

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='ora2',
-    version='2.11.4',
+    version='2.11.5',
     author='edX',
     author_email='oscm@edx.org',
     url='http://github.com/edx/edx-ora2',
