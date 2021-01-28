ff12af882aae47c92d53649cdf64bc6cc66ec899
jansenk <jkantor@edx.org>
2021-01-28 15:02:11 -0500

    remove student training from default assessments in which there isn't also peer

--- openassessment/xblock/test/test_defaults.py
+++ openassessment/xblock/test/test_defaults.py
@@ -12,11 +12,11 @@ from openassessment.xblock.validation import _is_valid_assessment_sequence as is
 class DefaultAssessmentTests(TestCase):
 
     @ddt.data(
-        (defaults.SELF_ASSESSMENT_MODULES, False),
+        (defaults.SELF_ASSESSMENT_MODULES, True),
         (defaults.PEER_ASSESSMENT_MODULES, True),
-        (defaults.STAFF_ASSESSMENT_MODULES, False),
+        (defaults.STAFF_ASSESSMENT_MODULES, True),
         (defaults.SELF_TO_PEER_ASSESSMENT_MODULES, True),
-        (defaults.SELF_TO_STAFF_ASSESSMENT_MODULES, False),
+        (defaults.SELF_TO_STAFF_ASSESSMENT_MODULES, True),
         (defaults.BLANK_ASSESSMENT_MODULES, False)
     )
     @ddt.unpack
