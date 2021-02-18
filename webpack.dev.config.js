90e4478abd30e4d5e8c16467c3aa6c372a2034f1
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2021-02-18 09:48:43 -0300

    Add missing rebase lines

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,6 +4,7 @@ process.env.NODE_ENV = 'development';
 const { createConfig } = require('@edx/frontend-build');
 const { mergeWithRules } = require('webpack-merge');
 
+const webpack = require('webpack');
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -67,15 +68,15 @@ config = mergeWithRules({
 
 Object.assign(config, {
   entry: {
-    "openassessment-lms": path.resolve(process.cwd(), 'openassessment/xblock/static/js/src/lms_index.js'),
-    "openassessment-studio": path.resolve(process.cwd(), 'openassessment/xblock/static/js/src/studio_index.js'),
-    "openassessment-rtl": path.resolve(process.cwd(), 'openassessment/xblock/static/sass/openassessment-rtl.scss'),
-    "openassessment-ltr": path.resolve(process.cwd(), 'openassessment/xblock/static/sass/openassessment-ltr.scss'),
-    "openassessment-editor-textarea": path.resolve(process.cwd(), 'openassessment/xblock/static/js/src/lms/editors/oa_editor_textarea.js'),
-    "openassessment-editor-tinymce": path.resolve(process.cwd(), 'openassessment/xblock/static/js/src/lms/editors/oa_editor_tinymce.js'),
+    'openassessment-lms': path.resolve(process.cwd(), 'openassessment/xblock/static/js/src/lms_index.js'),
+    'openassessment-studio': path.resolve(process.cwd(), 'openassessment/xblock/static/js/src/studio_index.js'),
+    'openassessment-rtl': path.resolve(process.cwd(), 'openassessment/xblock/static/sass/openassessment-rtl.scss'),
+    'openassessment-ltr': path.resolve(process.cwd(), 'openassessment/xblock/static/sass/openassessment-ltr.scss'),
+    'openassessment-editor-textarea': path.resolve(process.cwd(), 'openassessment/xblock/static/js/src/lms/editors/oa_editor_textarea.js'),
+    'openassessment-editor-tinymce': path.resolve(process.cwd(), 'openassessment/xblock/static/js/src/lms/editors/oa_editor_tinymce.js'),
   },
   output: {
-    path: path.resolve(process.cwd(), 'openassessment/xblock/static/compiled'),
+    path: path.resolve(process.cwd(), 'openassessment/xblock/static/dist'),
   },
   optimization: {},
   plugins: [
@@ -85,12 +86,15 @@ Object.assign(config, {
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
-    })
+    }),
+    new webpack.ProvidePlugin({
+      Backgrid: path.resolve(path.join(__dirname, 'openassessment/xblock/static/js/lib/backgrid/backgrid')),
+    }),
   ],
 });
 
 config.resolve.modules = ['node_modules'].concat(
-    path.resolve(__dirname, 'openassessment/xblock/static/js/src')
+  path.resolve(__dirname, 'openassessment/xblock/static/js/src'),
 );
 
 module.exports = config;
