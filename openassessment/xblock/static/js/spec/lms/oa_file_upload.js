ca62d26660b93d6a1c329a07063b5da685841d15
Nathan Sprenkle <nsprenkle@edx.org>
2021-02-19 15:31:24 -0500

    Update filename encoding to handle more characters

--- openassessment/xblock/static/js/spec/lms/oa_file_upload.js
+++ openassessment/xblock/static/js/spec/lms/oa_file_upload.js
@@ -11,6 +11,9 @@ describe("OpenAssessment.FileUploader", function() {
         type: "image/jpeg"
     };
 
+    var FILE_NAME_SPECIAL_CHARS = "tëšt|^`#,;?@&=+$-_.!~*'().exe.png";
+    var FILE_NAME_SPECIAL_ENCODED = "te%CC%88s%CC%8Ct|^`%23%2C%3B%3F%40%26%3D%2B%24-_.!~%2A%27%28%29.exe.png";
+
     beforeEach(function() {
         fileUploader = new FileUploader();
     });
@@ -36,7 +39,7 @@ describe("OpenAssessment.FileUploader", function() {
             async: false,
             processData: false,
             contentType: 'image/jpeg',
-            headers: { 'Content-Disposition': `attachment; filename*=UTF-8''${encodeURI(TEST_FILE.name)}` },
+            headers: { 'Content-Disposition': `attachment; filename*=UTF-8''${(TEST_FILE.name)}` },
         });
 
         // Verify that the event was logged
@@ -46,7 +49,31 @@ describe("OpenAssessment.FileUploader", function() {
                 fileSize: TEST_FILE.size,
                 fileType: TEST_FILE.type
             }
-        );
+        )
+    });
+
+    it("encodes file names correctly when uploading", function() {
+        // Stub the AJAX call, simulating success
+        var successPromise = $.Deferred(
+            function(defer) { defer.resolve(); }
+        ).promise();
+        spyOn($, 'ajax').and.returnValue(successPromise);
+
+        // Upload a file, with special and non-ASCII characters in the name
+        var specialCharacterFile = { ...TEST_FILE };
+        specialCharacterFile.name = FILE_NAME_SPECIAL_CHARS;
+        fileUploader.upload(TEST_URL, specialCharacterFile);
+
+        // Verify that a PUT request was sent with the right parameters
+        expect($.ajax).toHaveBeenCalledWith({
+            url: TEST_URL,
+            type: 'PUT',
+            data: specialCharacterFile,
+            async: false,
+            processData: false,
+            contentType: 'image/jpeg',
+            headers: { 'Content-Disposition': `attachment; filename*=UTF-8''${(FILE_NAME_SPECIAL_ENCODED)}` },
+        });
     });
 
     it("logs a file upload error event", function(done) {
