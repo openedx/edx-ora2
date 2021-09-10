8cd98cccd3b9cada580cd6b7e0924efade1a47ac
jansenk <jkantor@edx.org>
2021-09-10 11:09:26 -0400

    Add bulk assess view, refactor verify assessment data

--- openassessment/xblock/test/test_staff.py
+++ openassessment/xblock/test/test_staff.py
@@ -7,6 +7,7 @@ import copy
 import json
 
 from unittest.mock import Mock, patch
+import ddt
 
 from submissions import team_api as team_sub_api
 from openassessment.assessment.api import (
@@ -234,13 +235,13 @@ class TestStaffAssessment(StaffAssessmentTestBase):
         submission = xblock.create_submission(student_item, self.SUBMISSION)
 
         self.set_staff_access(xblock)
-        STAFF_GOOD_ASSESSMENT['submission_uuid'] = submission['uuid']
 
         for key in STAFF_GOOD_ASSESSMENT:
             # We don't want to fail if the assess_type is not submitted to the
             # backend, since it's only used for eventing right now.
             if key != 'assess_type':
                 assessment_copy = copy.copy(STAFF_GOOD_ASSESSMENT)
+                assessment_copy['submission_uuid'] = submission['uuid']
                 del assessment_copy[key]
                 resp = self.request(xblock, 'staff_assess', json.dumps(assessment_copy), response_format='json')
                 self.assertFalse(resp['success'])
@@ -254,7 +255,9 @@ class TestStaffAssessment(StaffAssessmentTestBase):
         submission = xblock.create_submission(student_item, self.SUBMISSION)
 
         self.set_staff_access(xblock)
-        STAFF_GOOD_ASSESSMENT['submission_uuid'] = submission['uuid']
+        assessment = copy.deepcopy(STAFF_GOOD_ASSESSMENT)
+        assessment['submission_uuid'] = submission['uuid']
+
         with patch('openassessment.xblock.staff_assessment_mixin.staff_api') as mock_api:
             #  Simulate a error
             mock_api.create_assessment.side_effect = staff_api.StaffAssessmentRequestError
@@ -267,6 +270,239 @@ class TestStaffAssessment(StaffAssessmentTestBase):
             resp = self.request(xblock, 'staff_assess', json.dumps(STAFF_GOOD_ASSESSMENT), response_format='json')
             self.assertFalse(resp['success'])
             self.assertIn('msg', resp)
+
+
+@ddt.ddt
+class TestBulkStaffAssessment(StaffAssessmentTestBase):
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.student_ids = [
+            "Alice",
+            "Billy",
+            "Cindy",
+            "Derek",
+            "Emma",
+            "Freddy"
+        ]
+
+    def _create_test_submissions(self, xblock):
+        test_submissions = {}
+        student_item = xblock.get_student_item_dict()
+        for student_id in self.student_ids:
+            copied_student_item = copy.deepcopy(student_item)
+            copied_student_item['student_id'] = student_id
+            student_submission = xblock.create_submission(
+                copied_student_item,
+                (f"{student_id}'s answer 1", f"{student_id}'s answer 2")
+            )
+            test_submissions[student_id] = student_submission
+        return test_submissions
+
+    def _build_assessment_dict(self, student_id, test_submissions):
+        """ Helper to construct an assessment dict from a copy of STAFF_GOOD_ASSESSMENT """
+        assessment = copy.deepcopy(STAFF_GOOD_ASSESSMENT)
+        assessment['submission_uuid'] = test_submissions[student_id]['uuid']
+        assessment['overall_feedback'] = f"overall feedback for {student_id}"
+        return assessment
+
+    @ddt.data(
+        ["Alice", "Billy", "Cindy", "Derek", "Emma", "Freddy"],
+        ["Alice", "Cindy", "Freddy"],
+        ["Derek"],  # the endpoint should still work even if the list is only one student long
+    )
+    @scenario('data/self_assessment_scenario.xml', user_id='Bob')
+    def test_staff_bulk_assess(self, xblock, target_students):
+        """ Test for normal behavior of staff bulk assess """
+        test_submissions = self._create_test_submissions(xblock)
+        submission_assessment_tuples = []
+        # Create assessments for all specified learners, with a custom "overall feedback"
+        for student_id in target_students:
+            assessment = self._build_assessment_dict(student_id, test_submissions)
+            submission_assessment_tuples.append((test_submissions[student_id], assessment))
+
+        self.submit_bulk_staff_assessment(xblock, *submission_assessment_tuples)
+
+        # Expect that a staff-assessment was created for each graded submission
+        for student_id in target_students:
+            submission = test_submissions[student_id]
+            assessment = staff_api.get_latest_staff_assessment(submission['uuid'])
+            self.assertIsNotNone(assessment)
+            self._assert_assessment_data_values(xblock, submission, student_id, assessment)
+
+        # Expect that submissions not included did not recieve a grade
+        for student_id, ungraded_submission in test_submissions.items():
+            if student_id in target_students:
+                continue
+            assessment = staff_api.get_latest_staff_assessment(ungraded_submission['uuid'])
+            self.assertIsNone(assessment)
+
+    def _assert_assessment_data_values(self, xblock, submission, student_id, assessment):
+        """ Helper to assert that the assessment data was saved correctly """
+        self.assertEqual(assessment['submission_uuid'], submission['uuid'])
+        self.assertEqual(assessment['points_earned'], 5)
+        self.assertEqual(assessment['points_possible'], 6)
+        self.assertEqual(assessment['scorer_id'], 'Bob')
+        self.assertEqual(assessment['score_type'], 'ST')
+        self.assertEqual(assessment['feedback'], f"overall feedback for {student_id}")
+
+        self.assert_assessment_event_published(
+            xblock, 'openassessmentblock.staff_assess', assessment, type='full-grade'
+        )
+
+        parts = assessment['parts']
+        parts.sort(key=lambda x: x['option']['name'])
+
+        self.assertEqual(len(parts), 2)
+        self.assertEqual(parts[0]['option']['criterion']['name'], 'Form')
+        self.assertEqual(parts[0]['option']['name'], 'Fair')
+        self.assertEqual(parts[1]['option']['criterion']['name'], '𝓒𝓸𝓷𝓬𝓲𝓼𝓮')
+        self.assertEqual(parts[1]['option']['name'], 'ﻉซƈﻉɭɭﻉกՇ')
+
+        # get the assessment scores by criteria
+        assessment_by_crit = staff_api.get_assessment_scores_by_criteria(submission["uuid"])
+        self.assertEqual(assessment_by_crit['𝓒𝓸𝓷𝓬𝓲𝓼𝓮'], 3)
+        self.assertEqual(assessment_by_crit['Form'], 2)
+
+        score = staff_api.get_score(submission["uuid"], None)
+        self.assertEqual(assessment['points_earned'], score['points_earned'])
+        self.assertEqual(assessment['points_possible'], score['points_possible'])
+
+    @scenario('data/self_assessment_scenario.xml', user_id='Bob')
+    def test_staff_assess_handler_regrade(self, xblock):
+        """ If different assess_types are included in one request, they should be reflected in trcking events. """
+        test_submissions = self._create_test_submissions(xblock)
+
+        alice_full_grade_assessment = copy.copy(STAFF_GOOD_ASSESSMENT)
+        alice_full_grade_assessment['overall_feedback'] = "full grade for Alice"
+
+        derek_regrade_assessment = copy.copy(STAFF_GOOD_ASSESSMENT)
+        derek_regrade_assessment['assess_type'] = 'regrade'
+        derek_regrade_assessment['overall_feedback'] = "regrade for Derek"
+
+        # Submit staff assessments
+        self.submit_bulk_staff_assessment(
+            xblock,
+            (test_submissions['Alice'], alice_full_grade_assessment),
+            (test_submissions['Derek'], derek_regrade_assessment),
+        )
+
+        alice_assessment = staff_api.get_latest_staff_assessment(test_submissions['Alice']['uuid'])
+        self.assert_assessment_event_published(
+            xblock, 'openassessmentblock.staff_assess', alice_assessment, type='full-grade'
+        )
+
+        derek_assessment = staff_api.get_latest_staff_assessment(test_submissions['Derek']['uuid'])
+        self.assert_assessment_event_published(
+            xblock, 'openassessmentblock.staff_assess', derek_assessment, type='regrade'
+        )
+
+    @scenario('data/self_assessment_scenario.xml', user_id='Bob')
+    def test_permission_error(self, xblock):
+        """ Test for error behavior when a user lcking permission requests the endpoint """
+        resp = self.request(xblock, 'bulk_staff_assess', json.dumps(STAFF_GOOD_ASSESSMENT))
+        self.assertIn("You do not have permission", resp.decode('utf-8'))
+
+    @scenario('data/self_assessment_scenario.xml', user_id='Bob')
+    def test_invalid_assessment_parameters(self, xblock):
+        """ Test for error behavior when inputs are invalid """
+        test_submissions = self._create_test_submissions(xblock)
+        self.set_staff_access(xblock)
+
+        def build_assessment_with_missing_key(student_id, key_to_delete):
+            assessment = self._build_assessment_dict(student_id, test_submissions)
+            del assessment[key_to_delete]
+            return assessment
+
+        alice_no_options_selected = build_assessment_with_missing_key("Alice", "options_selected")
+        billy_no_criterion_feedback = build_assessment_with_missing_key("Billy", "criterion_feedback")
+        cindy_no_overall_feedback = build_assessment_with_missing_key("Cindy", "overall_feedback")
+
+        derek_good_assessment = self._build_assessment_dict('Derek', test_submissions)
+
+        # Expect the response to fail and for the response to include error info
+        payload = [
+            alice_no_options_selected, billy_no_criterion_feedback, cindy_no_overall_feedback, derek_good_assessment
+        ]
+        resp = self.request(xblock, 'bulk_staff_assess', json.dumps(payload), response_format='json')
+        self.assertFalse(resp['success'])
+        self.assertEqual(resp['msg'], "One or more of the submitted assessments is missing required fields")
+        self.assertDictEqual(
+            resp['errors'],
+            {
+                '0': "You must provide options selected in the assessment.",
+                '1': "You must provide feedback for criteria in the assessment.",
+                '2': "You must provide overall feedback in the assessment.",
+            }
+        )
+        # The valid assessment was not included in errors, but was not processed
+        derek_assessment = staff_api.get_latest_staff_assessment(test_submissions['Derek']['uuid'])
+        self.assertIsNone(derek_assessment)
+
+    @scenario('data/self_assessment_scenario.xml', user_id='bob')
+    def test_assessment_error(self, xblock):
+        """ Test for error behavior when there are one or more errors submitting staff assessments """
+        test_submissions = self._create_test_submissions(xblock)
+        self.set_staff_access(xblock)
+
+        cindy_assessment = self._build_assessment_dict('Cindy', test_submissions)
+        derek_assessment = self._build_assessment_dict('Derek', test_submissions)
+        assessment_no_submission_uuid = copy.deepcopy(STAFF_GOOD_ASSESSMENT)
+
+        with patch('openassessment.xblock.staff_assessment_mixin.staff_api') as mock_api:
+            mock_api.create_assessment.side_effect = [
+                staff_api.StaffAssessmentRequestError, staff_api.StaffAssessmentInternalError, None
+            ]
+            payload = [cindy_assessment, derek_assessment, assessment_no_submission_uuid]
+            resp = self.request(xblock, 'bulk_staff_assess', json.dumps(payload), response_format='json')
+        self.assertFalse(resp['success'])
+        self.assertEqual(resp['msg'], "There were one or more errors submitting the requested assessments")
+        self.assertDictEqual(
+            resp['errors'],
+            {
+                '0': "Your staff assessment could not be submitted.",
+                '1': "Your staff assessment could not be submitted.",
+                '2': "The submission ID of the submission being assessed was not found.",
+            }
+        )
+
+    @scenario('data/self_assessment_scenario.xml', user_id='bob')
+    def test_assessment_mixed_error_and_success(self, xblock):
+        """ Test for error behavior when there are both failed and successful staff assessments """
+        test_submissions = self._create_test_submissions(xblock)
+        self.set_staff_access(xblock)
+
+        assessment_no_submission_uuid = copy.deepcopy(STAFF_GOOD_ASSESSMENT)
+        cindy_assessment = self._build_assessment_dict('Cindy', test_submissions)
+        derek_assessment = self._build_assessment_dict('Derek', test_submissions)
+
+        payload = [
+            assessment_no_submission_uuid,
+            cindy_assessment,
+            assessment_no_submission_uuid,
+            derek_assessment,
+            assessment_no_submission_uuid,
+            assessment_no_submission_uuid,
+        ]
+        resp = self.request(xblock, 'bulk_staff_assess', json.dumps(payload), response_format='json')
+        self.assertFalse(resp['success'])
+        self.assertEqual(resp['msg'], "There were one or more errors submitting the requested assessments")
+        self.assertDictEqual(
+            resp['errors'],
+            {
+                '0': "The submission ID of the submission being assessed was not found.",
+                '2': "The submission ID of the submission being assessed was not found.",
+                '4': "The submission ID of the submission being assessed was not found.",
+                '5': "The submission ID of the submission being assessed was not found.",
+            }
+        )
+
+        cindy_assessment = staff_api.get_latest_staff_assessment(test_submissions['Cindy']['uuid'])
+        self._assert_assessment_data_values(xblock, test_submissions['Cindy'], 'Cindy', cindy_assessment)
+
+        derek_assessment = staff_api.get_latest_staff_assessment(test_submissions['Derek']['uuid'])
+        self._assert_assessment_data_values(xblock, test_submissions['Derek'], 'Derek', derek_assessment)
 
 
 class TestStaffTeamAssessment(StaffAssessmentTestBase):
