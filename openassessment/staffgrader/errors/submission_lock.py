5cd78f7638963ead35309e04deb1c1faecd68861
Nathan Sprenkle <nsprenkle@edx.org>
2021-11-05 15:14:07 -0400

    refactor: return error code for lock contestation

--- openassessment/staffgrader/errors/submission_lock.py
+++ openassessment/staffgrader/errors/submission_lock.py
@@ -7,3 +7,7 @@ class SubmissionLockContestedError(Exception):
     """
     Error indicating trying to modify a lock that the user does not have access to modify.
     """
+    error_code = 'ERR_LOCK_CONTESTED'
+
+    def __str__(self):
+        return self.error_code
