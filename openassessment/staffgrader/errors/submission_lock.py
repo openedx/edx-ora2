6d9ed32ce80a26c99e7300402baf631a8ad3bff7
Nathan Sprenkle <nsprenkle@users.noreply.github.com>
2021-11-10 12:55:58 -0500

    feat: add lock status to submission lock responses (#1741)
    * feat: add lock status to lock serializer
    
    This has the side-effect of returning lock_status even when no lock is
    found. Helpful for frontend use.
    
    * refactor: return error code for lock contestation
    
    Engineering guidance is to return well-defined error codes instead of
    error strings to let the frontend match and worry about i18n and
    display of error messages.

--- openassessment/staffgrader/errors/submission_lock.py
+++ openassessment/staffgrader/errors/submission_lock.py
@@ -7,3 +7,7 @@ class SubmissionLockContestedError(Exception):
     """
     Error indicating trying to modify a lock that the user does not have access to modify.
     """
+    error_code = 'ERR_LOCK_CONTESTED'
+
+    def get_error_code(self):
+        return self.error_code
