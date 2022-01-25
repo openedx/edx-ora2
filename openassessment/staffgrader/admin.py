0983c701b9301c3699e957907d8d9f92b7e97a55
jansenk <jkantor@edx.org>
2022-01-25 11:56:27 -0500

    fixup! feat: add SubmissionGradingLockAdmin

--- openassessment/staffgrader/admin.py
+++ openassessment/staffgrader/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from openassessment.staffgrader.models import SubmissionGradingLock
 
+
 class SubmissionGradingLockAdmin(admin.ModelAdmin):
     """
     Django admin model for SubmissionGradingLock.
@@ -8,10 +9,11 @@ class SubmissionGradingLockAdmin(admin.ModelAdmin):
     list_display = ('id', 'submission_uuid', 'owner_id', 'created_at', 'is_active')
     readonly_fields = ('is_active',)
     search_fields = ('submission_uuid',)
-    
+
     # This allows us to have the nice boolean check/x icons in the list rather than "True"/"False"
     def is_active(self, lock):
         return lock.is_active
     is_active.boolean = True
-    
+
+
 admin.site.register(SubmissionGradingLock, SubmissionGradingLockAdmin)
