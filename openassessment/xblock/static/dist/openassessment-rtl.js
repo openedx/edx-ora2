7fc3d2b25486fac372f9cad1e6c6cfc8718d59f0
jansenk <jkantor@edx.org>
2022-02-24 12:38:17 -0500

    fix: trestore original tinymce behavior, fix trailing slash

--- openassessment/xblock/static/dist/openassessment-rtl.js
+++ openassessment/xblock/static/dist/openassessment-rtl.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9b4ef3cca7cc7fc55723";
+/******/ 	var hotCurrentHash = "2e3a21dfff587671eb8c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
