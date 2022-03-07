bba306b9b7f732ae0ae3510a834dc6be5e39917a
Nathan Sprenkle <nsprenkle@edx.org>
2022-03-07 16:42:22 -0500

    test: add tests for team list serialization

--- openassessment/staffgrader/tests/test_serializers.py
+++ openassessment/staffgrader/tests/test_serializers.py
@@ -3,6 +3,7 @@ Tests for serializers used in staff grading
 """
 from contextlib import contextmanager, ExitStack
 from datetime import datetime, timedelta, timezone
+import enum
 from typing import OrderedDict
 from uuid import uuid4
 
@@ -12,7 +13,7 @@ from freezegun import freeze_time
 from mock import Mock, patch
 
 from openassessment.staffgrader.serializers.submission_list import (
-    SubmissionListSerializer, SubmissionListScoreSerializer
+    SubmissionListSerializer, SubmissionListScoreSerializer, TeamSubmissionListSerializer
 )
 from openassessment.staffgrader.models.submission_lock import SubmissionGradingLock
 from openassessment.staffgrader.serializers.submission_lock import SubmissionLockSerializer
@@ -118,6 +119,11 @@ class TestSubmissionListSerializer(BaseSerializerTest):
             yield
 
     @contextmanager
+    def mock_get_teamName(self):
+        with patch.object(SubmissionListSerializer, 'get_teamName', return_value='get_teamName'):
+            yield
+
+    @contextmanager
     def mock_get_score(self):
         with patch.object(SubmissionListSerializer, 'get_score', return_value='get_score'):
             yield
@@ -128,12 +134,14 @@ class TestSubmissionListSerializer(BaseSerializerTest):
             yield
 
     @contextmanager
-    def mock_serializer_methods(self, gradedBy=False, username=False, score=False, verify=False):
+    def mock_serializer_methods(self, gradedBy=False, username=False, teamName=False, score=False, verify=False):
         with ExitStack() as stack:
             if gradedBy:
                 stack.enter_context(self.mock_get_gradedBy())
             if username:
                 stack.enter_context(self.mock_get_username())
+            if teamName:
+                stack.enter_context(self.mock_get_teamName())
             if score:
                 stack.enter_context(self.mock_get_score())
             if verify:
@@ -142,7 +150,7 @@ class TestSubmissionListSerializer(BaseSerializerTest):
 
     def test_serializer(self):
         mock_workflow = Mock()
-        with self.mock_serializer_methods(gradedBy=True, username=True, score=True, verify=True):
+        with self.mock_serializer_methods(gradedBy=True, username=True, teamName=True, score=True, verify=True):
             result = SubmissionListSerializer(mock_workflow).data
         self.assertDictEqual(
             result,
@@ -154,6 +162,7 @@ class TestSubmissionListSerializer(BaseSerializerTest):
                 'lockStatus': str(mock_workflow.lock_status),
                 'gradedBy': 'get_gradedBy',
                 'username': 'get_username',
+                'teamName': 'get_teamName',
                 'score': 'get_score',
             }
         )
@@ -222,6 +231,22 @@ class TestSubmissionListSerializer(BaseSerializerTest):
 
         self.assertEqual(result['username'], username)
 
+    def test_get_teamName(self):
+        mock_workflow = Mock()
+        student_id, username = 'test_student_id', 'test_username'
+
+        with self.mock_serializer_methods(gradedBy=True, username=True, score=True, verify=True):
+            result = SubmissionListSerializer(
+                mock_workflow,
+                context={
+                    'submission_uuid_to_student_id': {mock_workflow.identifying_uuid: student_id},
+                    'anonymous_id_to_username': {student_id: username}
+                }
+            ).data
+
+        # Team name should always be none for this individual responses
+        self.assertEqual(result['teamName'], None)
+
     def test_integration(self):
         # Make three workflows. The first two have scorer_ids and the third does not
         workflows = [
@@ -270,6 +295,7 @@ class TestSubmissionListSerializer(BaseSerializerTest):
                 'lockStatus': str(workflows[0].lock_status),
                 'gradedBy': 'staff_username_1',
                 'username': 'username_0',
+                'teamName': None,
                 'score': {
                     'pointsEarned': 10,
                     'pointsPossible': 20,
@@ -283,6 +309,7 @@ class TestSubmissionListSerializer(BaseSerializerTest):
                 'lockStatus': str(workflows[1].lock_status),
                 'gradedBy': 'staff_username_2',
                 'username': 'username_1',
+                'teamName': None,
                 'score': {
                     'pointsEarned': 7,
                     'pointsPossible': 20,
@@ -296,6 +323,189 @@ class TestSubmissionListSerializer(BaseSerializerTest):
                 'lockStatus': str(workflows[2].lock_status),
                 'gradedBy': None,
                 'username': 'username_2',
+                'teamName': None,
+                'score': {},
+            })
+        ]
+
+        self.assertEqual(data, expected_data)
+
+@ddt.ddt
+class TestTeamSubmissionListSerializer(BaseSerializerTest):
+    """Tests for serializing a list of team submissions"""
+
+    @contextmanager
+    def mock_get_gradedBy(self):
+        with patch.object(TeamSubmissionListSerializer, 'get_gradedBy', return_value='get_gradedBy'):
+            yield
+
+    @contextmanager
+    def mock_get_username(self):
+        with patch.object(TeamSubmissionListSerializer, 'get_username', return_value='get_username'):
+            yield
+
+    @contextmanager
+    def mock_get_teamName(self):
+        with patch.object(TeamSubmissionListSerializer, 'get_teamName', return_value='get_teamName'):
+            yield
+
+    @contextmanager
+    def mock_get_score(self):
+        with patch.object(TeamSubmissionListSerializer, 'get_score', return_value='get_score'):
+            yield
+
+    @contextmanager
+    def mock_verify_required_context(self):
+        with patch.object(TeamSubmissionListSerializer, '_verify_required_context'):
+            yield
+
+    @contextmanager
+    def mock_serializer_methods(self, gradedBy=False, username=False, teamName=False, score=False, verify=False):
+        with ExitStack() as stack:
+            if gradedBy:
+                stack.enter_context(self.mock_get_gradedBy())
+            if username:
+                stack.enter_context(self.mock_get_username())
+            if teamName:
+                stack.enter_context(self.mock_get_teamName())
+            if score:
+                stack.enter_context(self.mock_get_score())
+            if verify:
+                stack.enter_context(self.mock_verify_required_context())
+            yield
+
+    def test_serializer(self):
+        """Test connections between serializer fields and underlying functions"""
+        mock_workflow = Mock()
+        with self.mock_serializer_methods(gradedBy=True, username=True, teamName=True, score=True, verify=True):
+            result = TeamSubmissionListSerializer(mock_workflow).data
+        self.assertDictEqual(
+            result,
+            {
+                'submissionUuid': str(mock_workflow.submission_uuid),
+                'dateSubmitted': str(mock_workflow.created_at),
+                'dateGraded': str(mock_workflow.grading_completed_at),
+                'gradingStatus': str(mock_workflow.grading_status),
+                'lockStatus': str(mock_workflow.lock_status),
+                'gradedBy': 'get_gradedBy',
+                'username': 'get_username',
+                'teamName': 'get_teamName',
+                'score': 'get_score',
+            }
+        )
+
+    def test_get_username(self):
+        mock_workflow = Mock()
+        student_id, username = 'test_student_id', 'test_username'
+
+        with self.mock_serializer_methods(teamName=True, gradedBy=True, score=True, verify=True):
+            result = TeamSubmissionListSerializer(
+                mock_workflow,
+                context={
+                    'submission_uuid_to_student_id': {mock_workflow.identifying_uuid: student_id},
+                    'anonymous_id_to_username': {student_id: username}
+                }
+            ).data
+
+        # Username should be null for team submissions
+        self.assertEqual(result['username'], None)
+
+    def test_get_teamName(self):
+        mock_workflow = Mock()
+        team_id, team_name = 'test_team_id', 'test_team_name'
+
+        with self.mock_serializer_methods(gradedBy=True, username=True, score=True, verify=True):
+            result = TeamSubmissionListSerializer(
+                mock_workflow,
+                context={
+                    'team_submission_uuid_to_team_id': {mock_workflow.identifying_uuid: team_id},
+                    'team_id_to_team_name': {team_id: team_name}
+                }
+            ).data
+
+        self.assertEqual(result['teamName'], team_name)
+
+    def test_integration(self):
+        """Simple integration test to see that fields map correctly"""
+        # Create 3 workflows, 2 have scorers
+        workflows = [
+            Mock(scorer_id='staff_student_id_1'),
+            Mock(scorer_id='staff_student_id_2'),
+            Mock(scorer_id=None)
+        ]
+
+        # Dict from workflow uuids to team_id_{0,1,2}
+        team_submission_uuid_to_team_id = {
+            workflow.identifying_uuid: f'team_id_{i}'
+            for i, workflow in enumerate(workflows)
+        }
+
+        # Add mappings from team ID to team name
+        team_id_to_team_name = {
+            f'team_id_{i}': f'Team name {i}'
+            for i, _ in enumerate(workflows)
+        }
+
+        # Anonymous id to username used only for scorer ids
+        anonymous_id_to_username = {}
+        anonymous_id_to_username[workflows[0].scorer_id] = 'staff_username_1'
+        anonymous_id_to_username[workflows[1].scorer_id] = 'staff_username_2'
+
+        # Add assessments for the "scored" workflows
+        submission_uuid_to_assessment = {
+            workflows[0].identifying_uuid: Mock(points_possible=20, points_earned=10),
+            workflows[1].identifying_uuid: Mock(points_possible=20, points_earned=7),
+        }
+
+        data = TeamSubmissionListSerializer(
+            workflows,
+            context={
+                'anonymous_id_to_username': anonymous_id_to_username,
+                'submission_uuid_to_assessment': submission_uuid_to_assessment,
+                'team_submission_uuid_to_team_id': team_submission_uuid_to_team_id,
+                'team_id_to_team_name': team_id_to_team_name,
+            },
+            many=True
+        ).data
+
+        expected_data = [
+            OrderedDict({
+                'submissionUuid': str(workflows[0].submission_uuid),
+                'dateSubmitted': str(workflows[0].created_at),
+                'dateGraded': str(workflows[0].grading_completed_at),
+                'gradingStatus': str(workflows[0].grading_status),
+                'lockStatus': str(workflows[0].lock_status),
+                'gradedBy': 'staff_username_1',
+                'username': None,
+                'teamName': 'Team name 0',
+                'score': {
+                    'pointsEarned': 10,
+                    'pointsPossible': 20,
+                },
+            }),
+            OrderedDict({
+                'submissionUuid': str(workflows[1].submission_uuid),
+                'dateSubmitted': str(workflows[1].created_at),
+                'dateGraded': str(workflows[1].grading_completed_at),
+                'gradingStatus': str(workflows[1].grading_status),
+                'lockStatus': str(workflows[1].lock_status),
+                'gradedBy': 'staff_username_2',
+                'username': None,
+                'teamName': 'Team name 1',
+                'score': {
+                    'pointsEarned': 7,
+                    'pointsPossible': 20,
+                },
+            }),
+            OrderedDict({
+                'submissionUuid': str(workflows[2].submission_uuid),
+                'dateSubmitted': str(workflows[2].created_at),
+                'dateGraded': str(workflows[2].grading_completed_at),
+                'gradingStatus': str(workflows[2].grading_status),
+                'lockStatus': str(workflows[2].lock_status),
+                'gradedBy': None,
+                'username': None,
+                'teamName': 'Team name 2',
                 'score': {},
             })
         ]
