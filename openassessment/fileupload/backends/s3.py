1c79dee6a1d16070e70b0e335af17df80218c98c
Farhaan Bukhsh <farhaan@opencraft.com>
2023-07-12 13:07:03 +0530

    fix: Added region and signature for S3 bucket (#1928)
    For a region based bucket boto3 needs to know about region and signature which edx-ora2 doesn't know about.
    
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- openassessment/fileupload/backends/s3.py
+++ openassessment/fileupload/backends/s3.py
@@ -75,12 +75,18 @@ def _connect_to_s3():
     aws_access_key_id = getattr(settings, "AWS_ACCESS_KEY_ID", None)
     aws_secret_access_key = getattr(settings, "AWS_SECRET_ACCESS_KEY", None)
     endpoint_url = getattr(settings, "AWS_S3_ENDPOINT_URL", None)
+    signature_version = getattr(settings, "AWS_S3_SIGNATURE_VERSION", None)
+    region_name = getattr(settings, "AWS_S3_REGION_NAME", None)
 
     return boto3.client(
         "s3",
         aws_access_key_id=aws_access_key_id,
         aws_secret_access_key=aws_secret_access_key,
         endpoint_url=endpoint_url,
+        config=botocore.client.Config(
+            signature_version=signature_version,
+            region_name=region_name,
+        )
     )
 
 
