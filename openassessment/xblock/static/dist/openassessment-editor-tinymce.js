0e8983c08cb587f68cc57a741e87487fa7afa260
Leangseu Kim <lkim@edx.org>
2023-07-24 17:00:27 -0400

    chore: build static

--- openassessment/xblock/static/dist/openassessment-editor-tinymce.js
+++ openassessment/xblock/static/dist/openassessment-editor-tinymce.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6fd687af717d3a51ba5e";
+/******/ 	var hotCurrentHash = "67c0452a6401ab2ac4ce";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
