6cc7a60be59342db25cf3a6fb52682d9d622ad19
zubairshakoorarbisoft <zubair.shakoor@arbisoft.com>
2023-08-01 23:38:43 +0500

    fix: indented

--- openassessment/fileupload/urls.py
+++ openassessment/fileupload/urls.py
@@ -5,8 +5,14 @@ from django.urls import path
 from openassessment.fileupload import views_django_storage, views_filesystem
 
 urlpatterns = [
-    path('django/<path:key>/', views_django_storage.django_storage,
-    name='openassessment-django-storage'),
-    path('<path:key>/', views_filesystem.filesystem_storage,
-    name='openassessment-filesystem-storage'),
+    path(
+        'django/<path:key>/',
+        views_django_storage.django_storage,
+        name='openassessment-django-storage'
+        ),
+    path(
+        '<path:key>/',
+        views_filesystem.filesystem_storage,
+        name='openassessment-filesystem-storage'
+        ),
 ]
