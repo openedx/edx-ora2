ced60b3c491b0d00acc9294a63020f0a7573d250
jszewczulak <jszewczulak@2u.com>
2023-08-17 16:59:21 -0400

    feat: added API to perform batch ORA workflow update

---
+++ openassessment/tests/test_workflow_batch_update_api.py
@@ -0,0 +1,238 @@
+import datetime
+import logging
+from django.utils import timezone
+from mock import patch, MagicMock
+
+from openassessment.test_utils import CacheResetTest
+from openassessment.assessment.models import PeerWorkflow
+from submissions import api as sub_api
+from openassessment.assessment.api import peer as peer_api
+from openassessment.workflow import api as workflow_api
+from openassessment import workflow_batch_update_api as update_api
+
+logger = logging.getLogger(__name__)
+
+STUDENT_ITEM = {
+    "student_id": "Tim",
+    "course_id": "Demo_Course",
+    "item_id": "item_one",
+    "item_type": "Peer_Submission",
+}
+
+STEPS = ['peer', 'self']
+
+
+class TestWorkflowBatchUpdateAPI(CacheResetTest):
+
+    def test_get_blocked_peer_workflows(self):
+        tim_sub, tim = self._create_student_and_submission("Tim", "Tim's answer")
+        miles_sub, miles = self._create_student_and_submission("Miles", "Miles's answer")
+        pat_sub, pat = self._create_student_and_submission("Pat", "Pat's answer")
+
+        blocked = update_api.get_blocked_peer_workflows()
+        # we expect 0 blocked submissions as they were just created
+        self.assertEqual(len(blocked), 0)
+
+        # set Tim's submission create_at date to >7 days ago
+        pw_tim = PeerWorkflow.objects.get(student_id=tim["student_id"])
+        pw_tim.created_at = timezone.now() - datetime.timedelta(days=8)
+        pw_tim.save()
+        blocked = update_api.get_blocked_peer_workflows()
+        # we expect 1 blocked submission
+        self.assertEqual(len(blocked), 1)
+
+        # set Pat's submission create_at date to >7 days ago and set completed_at date
+        pw_pat = PeerWorkflow.objects.get(student_id=pat["student_id"])
+        pw_pat.created_at = timezone.now() - datetime.timedelta(days=8)
+        pw_pat.completed_at = timezone.now() - datetime.timedelta(days=2)
+        pw_pat.save()
+        blocked = update_api.get_blocked_peer_workflows()
+        # we still expect 1 blocked submission (Tim)
+        self.assertEqual(len(blocked), 1)
+        self.assertEqual(blocked[0].student_id, 'Tim')
+
+    def test_get_blocked_peer_workflows_for_course(self):
+        tim_sub, tim = self._create_student_and_submission("Tim", "Tim's answer")
+        miles_sub, miles = self._create_student_and_submission("Miles", "Miles's answer")
+        pat_sub, pat = self._create_student_and_submission("Pat", "Pat's answer")
+
+        blocked = update_api.get_blocked_peer_workflows_for_course("course_1")
+        # we expect 0 blocked submissions for "course_1"
+        self.assertEqual(len(blocked), 0)
+
+        pw_tim = PeerWorkflow.objects.get(student_id=tim["student_id"])
+        pw_tim.created_at = timezone.now() - datetime.timedelta(days=8)
+        pw_tim.save()
+
+        pw_pat = PeerWorkflow.objects.get(student_id=pat["student_id"])
+        pw_pat.created_at = timezone.now() - datetime.timedelta(days=8)
+        pw_pat.course_id = "course_1"
+        pw_pat.save()
+
+        blocked = update_api.get_blocked_peer_workflows_for_course("course_1")
+        # we expect 1 blocked submission for "course_1"
+        self.assertEqual(len(blocked), 1)
+
+        pw_miles = PeerWorkflow.objects.get(student_id=miles["student_id"])
+        pw_miles.course_id = "course_2"
+        pw_miles.save()
+
+        blocked = update_api.get_blocked_peer_workflows_for_course("course_2")
+        # we expect 0 blocked submissions for "course_2"
+        self.assertEqual(len(blocked), 0)
+
+    def test_get_blocked_peer_workflows_for_ora_block(self):
+        tim_sub, tim = self._create_student_and_submission("Tim", "Tim's answer")
+        miles_sub, miles = self._create_student_and_submission("Miles", "Miles's answer")
+        pat_sub, pat = self._create_student_and_submission("Pat", "Pat's answer")
+
+        blocked = update_api.get_blocked_peer_workflows_for_ora_block("item_1")
+        # we expect 0 blocked submissions for "item_1"
+        self.assertEqual(len(blocked), 0)
+
+        pw_tim = PeerWorkflow.objects.get(student_id=tim["student_id"])
+        pw_tim.created_at = timezone.now() - datetime.timedelta(days=8)
+        pw_tim.save()
+
+        pw_pat = PeerWorkflow.objects.get(student_id=pat["student_id"])
+        pw_pat.created_at = timezone.now() - datetime.timedelta(days=8)
+        pw_pat.item_id = "item_1"
+        pw_pat.save()
+
+        blocked = update_api.get_blocked_peer_workflows_for_ora_block("item_1")
+        # we expect 1 blocked submission for "item_1"
+        self.assertEquals(len(blocked), 1)
+
+        pw_miles = PeerWorkflow.objects.get(student_id=miles["student_id"])
+        pw_miles.item_id = "item_2"
+        pw_miles.save()
+
+        blocked = update_api.get_blocked_peer_workflows_for_ora_block("item_2")
+        # we expect 0 blocked submissions for "item_2"
+        self.assertEquals(len(blocked), 0)
+
+    def test_is_flexible_peer_grading_on(self):
+        workflow_requirements = {"peer": {
+            "must_grade": 2,
+            "must_be_graded_by": 3,
+            "enable_flexible_grading": True
+        }}
+        ora_block = MockOraBlock(workflow_requirements)
+        self.assertTrue(update_api.is_flexible_peer_grading_on(ora_block))
+
+        ora_block.requirements['peer']['enable_flexible_grading'] = False
+        self.assertFalse(update_api.is_flexible_peer_grading_on(ora_block))
+
+        ora_block.requirements['peer']['enable_flexible_grading'] = False
+        self.assertFalse(update_api.is_flexible_peer_grading_on(ora_block))
+
+        del (ora_block.requirements['peer']['enable_flexible_grading'])
+        self.assertFalse(update_api.is_flexible_peer_grading_on(ora_block))
+
+        del (ora_block.requirements['peer'])
+        self.assertFalse(update_api.is_flexible_peer_grading_on(ora_block))
+
+    @patch('openassessment.workflow.api.update_from_assessments')
+    def test_update_workflow_for_submission(self, mock_update_from_assessments):
+        mock_update_from_assessments.return_value = "workflow"
+
+        workflow = update_api.update_workflow_for_submission("submission_uuid", "assessment_requirements",
+                                                             "course_override")
+        mock_update_from_assessments.assert_called_once_with("submission_uuid", "assessment_requirements",
+                                                             "course_override")
+        self.assertEqual(workflow, "workflow")
+
+    @patch('openassessment.workflow_batch_update_api.update_workflow_for_submission')
+    def test_update_workflows(self, mock_update_workflow_for_submission):
+        update_api.update_workflows({})
+        mock_update_workflow_for_submission.assert_not_called()
+
+        update_api.update_workflows(None)
+        mock_update_workflow_for_submission.assert_not_called()
+
+        assessment_requirements_dict = {
+            "submission_uuid_1": {},
+            "submission_uuid_2": {},
+        }
+        update_api.update_workflows(assessment_requirements_dict)
+        mock_update_workflow_for_submission.assert_called_with("submission_uuid_2", {}, None)
+        self.assertEqual(mock_update_workflow_for_submission.call_count, 2)
+
+        # exceptions for individual updates should not disrupt batch process:
+        try:
+            mock_update_workflow_for_submission.side_effect = Exception()
+            update_api.update_workflows(assessment_requirements_dict)
+        except Exception as e:
+            self.fail("Exception not expected")
+
+    @patch('openassessment.workflow_batch_update_api.update_workflows')
+    @patch('openassessment.workflow_batch_update_api.get_blocked_peer_workflows_for_ora_block')
+    @patch('openassessment.workflow_batch_update_api.get_assessment_requirements_for_flex_peer_grading')
+    def test_update_workflows_for_ora_block(self, mock_update_workflows,
+                                            mock_get_blocked_peer_workflows_for_ora_block,
+                                            mock_get_assessment_requirements_for_flex_peer_grading):
+        update_api.update_workflows_for_ora_block("some_item_id")
+        mock_get_blocked_peer_workflows_for_ora_block.assert_called_once_with("some_item_id")
+        mock_get_assessment_requirements_for_flex_peer_grading.assert_called_once()
+        mock_update_workflows.assert_called_once()
+
+        # exceptions should not disrupt batch process:
+        try:
+            mock_get_blocked_peer_workflows_for_ora_block.side_effect = Exception()
+            update_api.update_workflows_for_ora_block("some_item_id")
+        except Exception as e:
+            self.fail("Exception not expected")
+
+    @patch('openassessment.workflow_batch_update_api.update_workflows')
+    @patch('openassessment.workflow_batch_update_api.get_blocked_peer_workflows_for_course')
+    @patch('openassessment.workflow_batch_update_api.get_assessment_requirements_for_flex_peer_grading')
+    def test_update_workflows_for_course(self, mock_update_workflows,
+                                         mock_get_blocked_peer_workflows_for_course,
+                                         mock_get_assessment_requirements_for_flex_peer_grading):
+        update_api.update_workflows_for_course("some_course_id")
+        mock_get_blocked_peer_workflows_for_course.assert_called_once_with("some_course_id")
+        mock_get_assessment_requirements_for_flex_peer_grading.assert_called_once()
+        mock_update_workflows.assert_called_once()
+
+        # exceptions should not disrupt batch process:
+        try:
+            mock_get_blocked_peer_workflows_for_course.side_effect = Exception()
+            update_api.update_workflows_for_ora_block("some_item_id")
+        except Exception as e:
+            self.fail("Exception not expected")
+
+    @patch('openassessment.workflow_batch_update_api.update_workflows')
+    @patch('openassessment.workflow_batch_update_api.get_blocked_peer_workflows')
+    @patch('openassessment.workflow_batch_update_api.get_assessment_requirements_for_flex_peer_grading')
+    def test_update_workflows_for_all_blocked_submissions(self, mock_update_workflows,
+                                                          mock_get_blocked_peer_workflows,
+                                                          mock_get_assessment_requirements_for_flex_peer_grading):
+        update_api.update_workflows_for_all_blocked_submissions()
+        mock_get_blocked_peer_workflows.assert_called_once()
+        mock_get_assessment_requirements_for_flex_peer_grading.assert_called_once()
+        mock_update_workflows.assert_called_once()
+
+        # exceptions should not disrupt batch process:
+        try:
+            mock_get_blocked_peer_workflows.side_effect = Exception()
+            update_api.update_workflows_for_all_blocked_submissions()
+        except Exception as e:
+            self.fail("Exception not expected")
+
+    @staticmethod
+    def _create_student_and_submission(student, answer, date=None, steps=None):
+        """ Creats a student and submission for tests. """
+        new_student_item = STUDENT_ITEM.copy()
+        new_student_item["student_id"] = student
+        submission = sub_api.create_submission(new_student_item, answer, date)
+        peer_api.on_start(submission["uuid"])
+        workflow_api.create_workflow(submission["uuid"], steps or STEPS)
+        return submission, new_student_item
+
+
+class MockOraBlock(object):
+    def __init__(self, requirements):
+        self.requirements = requirements
+
+    def workflow_requirements(self):
+        return self.requirements
---
+++ openassessment/workflow_batch_update_api.py
@@ -0,0 +1,246 @@
+"""
+Provides functionality to batch update ORA workflows for different scopes
+"""
+
+import logging
+import time
+from django.utils import timezone
+import datetime
+from celery import shared_task
+
+from opaque_keys.edx.keys import UsageKey
+from openassessment.runtime_imports.functions import modulestore
+from openassessment.assessment.models import PeerWorkflow
+from openassessment.workflow import api
+
+logger = logging.getLogger(__name__)
+
+@shared_task
+def update_workflow_for_submission(submission_uuid, assessment_requirements, course_override):
+    """
+    Wrapper for `workflow.api.update_from_assessments(submission_uuid, assessment_requirements, course_override)`
+    """
+    start = time.time()
+    workflow = api.update_from_assessments(submission_uuid, assessment_requirements, course_override)
+    end = time.time()
+    logger.info(
+        "Update_workflow_for_submission completed successfully; submission_uuid=%s assessment_requirements=%s course_setttings=%s processing_time=%s",
+        submission_uuid,
+        assessment_requirements,
+        course_override,
+        str(end - start))
+    return workflow
+
+@shared_task
+def update_workflows_for_ora_block(item_id):
+    """
+    Updates ORA workflows created for the given ORA Block
+
+    Args:
+    item_id (str): Identifier for the ORA Block
+        e.g. 'block-v1:edX+DemoX+Demo_Course+type@openassessment+block@1676f4b05f0642249ff724e7c07d869e'
+    """
+    try:
+        start = time.time()
+
+        peer_workflows = get_blocked_peer_workflows_for_ora_block(item_id)
+        assessment_requirements_dict = get_assessment_requirements_for_flex_peer_grading(peer_workflows)
+        update_workflows(assessment_requirements_dict)
+
+        end = time.time()
+        logger.info(
+            "Batch workflow update for ORA block submissions completed successfully; item_id=%s ;  processing_time=%s",
+            item_id,
+            str(end - start))
+
+    except Exception as e:
+        logger.error(
+            "Batch workflow update. Error occurred while updating workflows for ORA block submissions. item_id=%s  Error:%s",
+            item_id,
+            str(e))
+
+@shared_task
+def update_workflows_for_course(course_id):
+    """
+    Updates ORA workflows created for the given course
+
+    Args:
+        course_id (str): Course identifier
+    """
+    try:
+        start = time.time()
+
+        peer_workflows = get_blocked_peer_workflows_for_course(course_id)
+        assessment_requirements_dict = get_assessment_requirements_for_flex_peer_grading(peer_workflows)
+        update_workflows(assessment_requirements_dict)
+
+        end = time.time()
+        logger.info(
+            "Batch workflow update for course submissions completed successfully; course_id=%s ;  processing_time=%s",
+            course_id,
+            str(end - start))
+
+    except Exception as e:
+        logger.error(
+            "Batch workflow update. Error occurred while updating workflows for all blocked submissions. course_id=%s Error:%s",
+            course_id,
+            str(e))
+
+@shared_task
+def update_workflows_for_all_blocked_submissions():
+    """
+    Updates ORA workflows for submissions meeting following filtering criteria:
+     - Flexible Peer Grading ON
+     - ungraded submissions that are >7 days old
+    """
+    try:
+        start = time.time()
+
+        peer_workflows = get_blocked_peer_workflows()
+        assessment_requirements_dict = get_assessment_requirements_for_flex_peer_grading(peer_workflows)
+        update_workflows(assessment_requirements_dict)
+
+        end = time.time()
+        logger.info(
+            "Batch workflow update for all blocked submissions completed successfully; processing_time=%s",
+            str(end - start))
+
+    except Exception as e:
+        logger.error(
+            "Batch workflow update. Error occurred while updating workflows for all blocked submissions.  Error:%s",
+            str(e))
+
+
+def update_workflows(assessment_requirements_dict):
+    """
+    Updates ORA workflows for the provided submission uuids provides in dictionary
+
+    Args:
+        assessment_requirements_dict (dict): Dictionary:   <submission_uuid>:<assessment_requirements>
+    """
+
+    if assessment_requirements_dict is not None:
+
+        for submission_uuid, assessment_requirements in assessment_requirements_dict.items():
+            try:
+                update_workflow_for_submission(submission_uuid, assessment_requirements, None)
+            except Exception as e:
+                logger.warning(
+                    "Batch workflow update. Error occurred while updating workflow for submission_uuid=%s assessment_requirements=%s   Error:%s",
+                    submission_uuid, assessment_requirements, str(e))
+
+
+def is_flexible_peer_grading_on(openassessmentblock):
+    """
+    Is flexible peer grading set "ON" for provided ORA block
+
+    Args:
+        openassessmentblock (OpenAssessmentBlock): ORA block
+
+    Returns:
+        bool: True if given ORA is configured with flexible peer grading set "ON"
+    """
+    workflow_requirements = openassessmentblock.workflow_requirements()
+    if workflow_requirements.get('peer') and workflow_requirements['peer'].get('enable_flexible_grading'):
+        return True
+
+    return False
+
+
+def get_blocked_peer_workflows():
+    """
+    Retrieve ORA peer workflows not completed for >7 days
+
+    Returns:
+        list (PeerWorkflow): list of workfows not completed for > 7 days
+    """
+
+    peer_workflows = PeerWorkflow.objects.filter(
+        created_at__lte=(timezone.now() - datetime.timedelta(days=7))
+    ).exclude(
+        completed_at__isnull=False
+    )
+    return peer_workflows
+
+
+def get_blocked_peer_workflows_for_course(course_id):
+    """
+    Retrieve ORA peer workflows not completed for >7 days for a given course
+
+    Args:
+        course_id (str): course identifier
+
+    Returns:
+        list (PeerWorkflow): list of workfows not completed for > 7 days
+    """
+    peer_workflows = PeerWorkflow.objects.filter(
+        created_at__lte=(timezone.now() - datetime.timedelta(days=7)),
+        course_id__exact=course_id
+    ).exclude(
+        completed_at__isnull=False
+    )
+    return peer_workflows
+
+
+def get_blocked_peer_workflows_for_ora_block(item_id):
+    """
+    Retrieve ORA peer workflows not completed for >7 days for a given ORA block id
+
+    Args:
+        item_id (str): Identifier of the ORA Block
+            e.g. 'block-v1:edX+DemoX+Demo_Course+type@openassessment+block@1676f4b05f0642249ff724e7c07d869e'
+
+    Returns:
+        list (PeerWorkflow): list of workfows not completed for > 7 days
+    """
+
+    peer_workflows = PeerWorkflow.objects.filter(
+        created_at__lte=(timezone.now() - datetime.timedelta(days=7)),
+        item_id__exact=item_id
+    ).exclude(
+        completed_at__isnull=False
+    )
+    return peer_workflows
+
+
+def get_assessment_requirements_for_flex_peer_grading(peer_workflows):
+    """
+    Returns assessment requirements for each provided peer workflow that was created
+    for ORA with flexible peer grading configured (ON) .
+
+    Args:
+        peer_workflows (list): list of `PeerWorkflow` objects
+
+    Returns:
+        assessment_requirements_dict (dict): Dictionary:  <submission_uuid>:<assessment_requirements>
+    """
+    # dictionary <submission_uuid>:<workflow_requirements>
+    workflow_requirements = {}
+    store = modulestore()
+
+    for peer_workflow in peer_workflows:
+        try:
+            ora_block = _get_ora_block(peer_workflow, store)
+
+            if is_flexible_peer_grading_on(ora_block):
+                workflow_requirements[peer_workflow.submission_uuid] = ora_block.workflow_requirements()
+        except Exception as e:
+            logger.warning(
+                "Batch workflow update. Error occurred while retrieving workflow requirements for open assessment: %s   Error:%s",
+                peer_workflow.item_id, str(e))
+
+    return workflow_requirements
+
+
+def _get_ora_block(peer_workflow, store=None):
+    """
+    Returns ORA block for the given PeerWorkflow
+    """
+    if store is None:
+        store = modulestore()
+
+    if peer_workflow is not None:
+        block_key = UsageKey.from_string(peer_workflow.item_id)
+        return store.get_item(block_key)
+
+    return None
