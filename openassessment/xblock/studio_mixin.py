047bde354827a212106132cc549e179cb6ba885c
Leangseu Kim <lkim@edx.org>
2023-08-23 14:34:30 -0400

    chore: linting

--- openassessment/xblock/studio_mixin.py
+++ openassessment/xblock/studio_mixin.py
@@ -56,7 +56,8 @@ class StudioMixin:
 
     STUDIO_EDITING_TEMPLATE = 'openassessmentblock/edit/oa_edit.html'
 
-    ORA_SETTINGS_DOCUMENT_URL = 'https://edx.readthedocs.io/projects/edx-partner-course-staff/en/latest/exercises_tools/open_response_assessments/CreateORAAssignment.html#specify-a-name-and-dates'  # pylint: disable=line-too-long
+    # pylint: disable-next=line-too-long
+    ORA_SETTINGS_DOCUMENT_URL = 'https://edx.readthedocs.io/projects/edx-partner-course-staff/en/latest/exercises_tools/open_response_assessments/CreateORAAssignment.html#specify-a-name-and-dates'
 
     BASE_EDITOR_ASSESSMENTS_ORDER = copy.deepcopy(DEFAULT_EDITOR_ASSESSMENTS_ORDER)
 
