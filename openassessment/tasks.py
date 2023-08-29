87bcf7f4ed2ec72441bf41d2c3fcdd4c5484fc7e
jszewczulak <jszewczulak@2u.com>
2023-08-29 15:19:57 -0400

    feat: added async tasks layer module

--- openassessment/tasks.py
+++ openassessment/tasks.py
@@ -8,11 +8,12 @@ from openassessment import workflow_batch_update_api
 def update_workflows_for_all_blocked_submissions():
     return workflow_batch_update_api.update_workflows_for_all_blocked_submissions()
 
+
 @shared_task
 def update_workflows_for_ora_block(item_id):
     return workflow_batch_update_api.update_workflows_for_ora_block(item_id)
 
+
 @shared_task
 def update_workflows_for_course(course_id):
     return workflow_batch_update_api.update_workflows_for_course(course_id)
-
