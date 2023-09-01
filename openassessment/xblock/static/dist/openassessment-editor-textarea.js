796f50fb641e0cd63d9b479630e931e29a2f9b27
jansenk <jkantor@edx.org>
2023-09-01 11:16:55 -0400

    chore: build static

--- openassessment/xblock/static/dist/openassessment-editor-textarea.js
+++ openassessment/xblock/static/dist/openassessment-editor-textarea.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "593efe10c094218a188a";
+/******/ 	var hotCurrentHash = "9f178f80778ef11c1723";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
