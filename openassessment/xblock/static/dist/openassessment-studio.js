807f0bdd9999eb6efc4b0be1638ffc4c1967b6c1
Jansen Kantor <jkantor@edx.org>
2023-09-05 14:17:32 -0400

    feat: add icons to draw attention to date config (#2040)
    * feat: add icons to draw attention to date config
    
    * chore: make static
    
    * chore: translations
    
    * docs: expand on removal todo
    
    * style: change icon to star
    
    * style: update colors
    
    * chore: build static
    
    * chore: translations
    
    * chore: version

--- openassessment/xblock/static/dist/openassessment-studio.js
+++ openassessment/xblock/static/dist/openassessment-studio.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b9fa3a3189564636ae2f";
+/******/ 	var hotCurrentHash = "9f178f80778ef11c1723";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
