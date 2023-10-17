0c8aafaaccb87e76f4b7ab4103627d0137a9e3fa
Leangseu Kim <lkim@edx.org>
2023-10-17 12:53:49 -0400

    chore: fix studio base view

--- openassessment/xblock/openassessmentblock.py
+++ openassessment/xblock/openassessmentblock.py
@@ -565,7 +565,7 @@ class OpenAssessmentBlock(
             "title": self.title,
             "xblock_id": self.get_xblock_id(),
         }
-        template = get_template("base.html")
+        template = get_template("openassessmentblock/base.html")
         return self._create_fragment(template, context_dict, initialize_js_func='OpenAssessmentBlock')
 
     @togglable_mobile_support
@@ -611,7 +611,8 @@ class OpenAssessmentBlock(
             "title": self.title,
             "xblock_id": self.get_xblock_id(),
         }
-        template = get_template("base.html")
+        # template = get_template("legacy/oa_base.html")
+        template = get_template("openassessmentblock/base.html")
         return self._create_fragment(template, context_dict, initialize_js_func='OpenAssessmentBlock')
 
     def ora_blocks_listing_view(self, context=None):
