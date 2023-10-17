1e6bad447735286d0003c6e9e8125119fd0569cf
Leangseu Kim <lkim@edx.org>
2023-10-17 13:31:20 -0400

    chore: remove unnecessary comment

--- openassessment/xblock/openassessmentblock.py
+++ openassessment/xblock/openassessmentblock.py
@@ -611,7 +611,6 @@ class OpenAssessmentBlock(
             "title": self.title,
             "xblock_id": self.get_xblock_id(),
         }
-        # template = get_template("legacy/oa_base.html")
         template = get_template("openassessmentblock/base.html")
         return self._create_fragment(template, context_dict, initialize_js_func='OpenAssessmentBlock')
 
