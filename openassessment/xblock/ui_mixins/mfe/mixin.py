ae64ad6f9ba8179458140ee83689f11b8aa280a2
nsprenkle <nsprenkle@2u.com>
2023-11-16 16:35:17 -0500

    style: fix linting issues

--- openassessment/xblock/ui_mixins/mfe/mixin.py
+++ openassessment/xblock/ui_mixins/mfe/mixin.py
@@ -327,7 +327,7 @@ class MfeMixin:
     def _assessment_get_peer_handler(self):
 
         # Raise an exception if we don't have a peer step
-        if not "peer-assessment" in self.assessment_steps:
+        if "peer-assessment" not in self.assessment_steps:
             raise OraApiException(400, error_codes.INACCESSIBLE_STEP, error_context="No peer step for ORA")
 
         # Call get_peer_submission to grab a new peer submission
