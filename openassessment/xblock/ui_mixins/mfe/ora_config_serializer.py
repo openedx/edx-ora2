1dea386a81bed3f61eae3039ccdad024d2451cac
jansenk <jkantor@edx.org>
2024-02-12 09:49:11 -0500

    fix: date config should work correctly on the mfe

--- openassessment/xblock/ui_mixins/mfe/ora_config_serializer.py
+++ openassessment/xblock/ui_mixins/mfe/ora_config_serializer.py
@@ -4,7 +4,6 @@ Serializers for ORA's BFF.
 These are the response shapes that power the MFE implementation of the ORA UI.
 """
 # pylint: disable=abstract-method
-
 from rest_framework.serializers import (
     BooleanField,
     DateTimeField,
@@ -13,6 +12,8 @@ from rest_framework.serializers import (
     CharField,
     SerializerMethodField,
 )
+
+from openassessment.xblock.utils.resolve_dates import DISTANT_FUTURE, DISTANT_PAST
 from openassessment.xblock.apis.workflow_api import WorkflowStep
 
 from openassessment.xblock.ui_mixins.mfe.serializer_utils import (
@@ -61,13 +62,30 @@ class TeamsConfigSerializer(Serializer):
 
 
 class SubmissionConfigSerializer(Serializer):
-    startDatetime = DateTimeField(source="submission_start")
-    endDatetime = DateTimeField(source="submission_due")
+    startDatetime = SerializerMethodField()
+    endDatetime = SerializerMethodField()
+    _date_range = None
 
     textResponseConfig = TextResponseConfigSerializer(source="*")
     fileResponseConfig = FileResponseConfigSerializer(source="*")
 
     teamsConfig = TeamsConfigSerializer(source="*")
+
+    def _get_start_end_date(self, xblock):
+        """ Cached calculation of step due dates """
+        if self._date_range is None:
+            _, _, start, end = xblock.is_closed(step='submission')
+            self._date_range = (
+                start.isoformat() if start > DISTANT_PAST else None,
+                end.isoformat() if end < DISTANT_FUTURE else None,
+            )
+        return self._date_range
+
+    def get_startDatetime(self, xblock):
+        return self._get_start_end_date(xblock)[0]
+
+    def get_endDatetime(self, xblock):
+        return self._get_start_end_date(xblock)[1]
 
 
 class RubricFeedbackConfigSerializer(Serializer):
@@ -111,36 +129,6 @@ class RubricConfigSerializer(Serializer):
     )
 
 
-class SelfSettingsSerializer(Serializer):
-    required = BooleanField(default=True)
-
-    startDatetime = DateTimeField(source="start")
-    endDatetime = DateTimeField(source="due")
-
-
-class StudentTrainingSettingsSerializer(Serializer):
-    required = BooleanField(default=True)
-
-    numberOfExamples = SerializerMethodField(source="*", default=0)
-
-    def get_numberOfExamples(self, assessment):
-        return len(assessment["examples"])
-
-
-class PeerSettingsSerializer(Serializer):
-    required = BooleanField(default=True)
-
-    startDatetime = DateTimeField(source="start")
-    endDatetime = DateTimeField(source="due")
-
-    minNumberToGrade = IntegerField(source="must_grade")
-    minNumberToBeGradedBy = IntegerField(source="must_be_graded_by")
-
-    enableFlexibleGrading = BooleanField(
-        source="enable_flexible_grading", required=False
-    )
-
-
 class AssessmentStepSettingsSerializer(Serializer):
     """
     Generic Assessments step, where we just need to know if the step is
@@ -148,6 +136,7 @@ class AssessmentStepSettingsSerializer(Serializer):
     """
 
     required = BooleanField(default=True)
+    STEP_NAME = None  # Overridden by child classes
 
     def _get_step(self, rubric_assessments, step_name):
         """Get the assessment step config for a given step_name"""
@@ -156,42 +145,60 @@ class AssessmentStepSettingsSerializer(Serializer):
                 return step
         return None
 
-    def __init__(self, *args, **kwargs):
-        self.step_name = kwargs.pop("step_name")
-        super().__init__(*args, **kwargs)
-
-    def to_representation(self, instance):
-        assessment_step = self._get_step(instance, self.step_name)
-
-        # Special handling for the peer step which includes extra fields
-        if assessment_step and self.step_name == "peer-assessment":
-            return PeerSettingsSerializer(assessment_step).data
-        elif assessment_step and self.step_name == "self-assessment":
-            return SelfSettingsSerializer(assessment_step).data
-        elif assessment_step and self.step_name == "student-training":
-            return StudentTrainingSettingsSerializer(assessment_step).data
-
+    # pylint: disable=arguments-renamed
+    def to_representation(self, xblock):
+        assessment_step = self._get_step(xblock.rubric_assessments, self.STEP_NAME)
         # If we didn't find a step, it is not required
         if assessment_step is None:
-            assessment_step = {"required": False}
+            return {"required": False}
+
+        assessment_step = dict(assessment_step)
+        # Add overridden start and due dates for peer assessment and self assessment
+        if self.STEP_NAME in ('peer-assessment', 'self-assessment'):
+            _, _, start, due = xblock.is_closed(step=self.STEP_NAME)
+            assessment_step['start'] = start.isoformat()
+            assessment_step['due'] = due.isoformat()
 
         return super().to_representation(assessment_step)
 
 
+class SelfSettingsSerializer(AssessmentStepSettingsSerializer):
+    STEP_NAME = 'self-assessment'
+    startDatetime = DateTimeField(source='start')
+    endDatetime = DateTimeField(source='due')
+
+
+class StudentTrainingSettingsSerializer(AssessmentStepSettingsSerializer):
+    STEP_NAME = 'student-training'
+    numberOfExamples = SerializerMethodField(source="*", default=0)
+
+    def get_numberOfExamples(self, assessment):
+        return len(assessment["examples"])
+
+
+class PeerSettingsSerializer(AssessmentStepSettingsSerializer):
+    STEP_NAME = 'peer-assessment'
+    minNumberToGrade = IntegerField(source="must_grade")
+    minNumberToBeGradedBy = IntegerField(source="must_be_graded_by")
+
+    startDatetime = DateTimeField(source='start')
+    endDatetime = DateTimeField(source='due')
+
+    enableFlexibleGrading = BooleanField(
+        source="enable_flexible_grading", required=False
+    )
+
+
+class StaffSettingsSerializer(AssessmentStepSettingsSerializer):
+    STEP_NAME = 'staff-assessment'
+
+
 class AssessmentStepsSettingsSerializer(Serializer):
-    studentTraining = AssessmentStepSettingsSerializer(
-        step_name="student-training", source="rubric_assessments"
-    )
-    peer = AssessmentStepSettingsSerializer(
-        step_name="peer-assessment", source="rubric_assessments"
-    )
+    studentTraining = StudentTrainingSettingsSerializer(source="*")
+    peer = PeerSettingsSerializer(source="*")
     # Workaround to allow reserved keyword in serializer key
-    vars()["self"] = AssessmentStepSettingsSerializer(
-        step_name="self-assessment", source="rubric_assessments"
-    )
-    staff = AssessmentStepSettingsSerializer(
-        step_name="staff-assessment", source="rubric_assessments"
-    )
+    vars()["self"] = SelfSettingsSerializer(source='*')
+    staff = StaffSettingsSerializer(source='*')
 
 
 class AssessmentStepsSerializer(Serializer):
