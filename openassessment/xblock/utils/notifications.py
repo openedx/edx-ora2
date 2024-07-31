0b7c4f9e34bf3ab98724222385c80eb2dacdd86d
eemaanamir <eemaan.amir@gmail.com>
2024-07-31 11:53:30 +0500

    fix: course name missing from ORA notification context

--- openassessment/xblock/utils/notifications.py
+++ openassessment/xblock/utils/notifications.py
@@ -6,6 +6,7 @@ import logging
 from django.conf import settings
 from openedx_events.learning.signals import COURSE_NOTIFICATION_REQUESTED
 from openedx_events.learning.data import CourseNotificationData
+from xmodule.modulestore.django import modulestore
 
 logger = logging.getLogger(__name__)
 
@@ -18,10 +19,12 @@ def send_staff_notification(course_id, problem_id, ora_name):
         audience_filters = {
             'course_roles': ['staff', 'instructor']
         }
+        course = modulestore().get_course(course_id)
         notification_data = CourseNotificationData(
             course_key=course_id,
             content_context={
-                'ora_name': ora_name
+                'ora_name': ora_name,
+                'course_name': course.display_name,
             },
             notification_type='ora_staff_notification',
             content_url=f"{getattr(settings, 'ORA_GRADING_MICROFRONTEND_URL', '')}/{problem_id}",
