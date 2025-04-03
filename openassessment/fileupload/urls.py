a7597317ab38db5150e9227477f49cd877c2a725
Jillian Vogel <jill@opencraft.com>
2025-04-03 16:42:14 +1030

    chore: support upgrading to django 5.2
    * Ran django-upgrade and updated migration
    * Added django52 target to tox & CI
    * Added Django 5.2 to setup docs
    * Fixed quality

--- openassessment/fileupload/urls.py
+++ openassessment/fileupload/urls.py
@@ -1,12 +1,12 @@
 """ Urls for fileupload app. """
 
-from django.urls import re_path
+from django.urls import path
 
 from openassessment.fileupload import views_django_storage, views_filesystem
 
 urlpatterns = [
-    re_path(r'^django/(?P<key>.+)/$', views_django_storage.django_storage,
-            name='openassessment-django-storage'),
-    re_path(r'^(?P<key>.+)/$', views_filesystem.filesystem_storage,
-            name='openassessment-filesystem-storage'),
+    path('django/<path:key>/', views_django_storage.django_storage,
+         name='openassessment-django-storage'),
+    path('<path:key>/', views_filesystem.filesystem_storage,
+         name='openassessment-filesystem-storage'),
 ]
