fdbeec9326468d2d6789cad1c8d40fec8cea1a66
Tarun Tak <ttak-apphelix@2u.com>
2026-01-22 11:04:57 +0000

    fix: update webpack configuration for test environment

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration
-const webpackConfig = require('./webpack.dev.config.js');
+const webpackConfig = require('./webpack.test.config.js');
 
 module.exports = function(config) {
   config.set({
